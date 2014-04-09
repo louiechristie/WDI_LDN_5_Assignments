4475e378d2d8fc1814c44e461411829a644e45ba
jasonklein <jasonklein@gmail.com>
2014-04-09 23:14:16 +0100

    Add mortgage method and slight other changes to previous work.

--- jason_banks/w2d3/calcit-homework/main.rb
+++ jason_banks/w2d3/calcit-homework/main.rb
@@ -7,15 +7,15 @@ get '/' do
 end
 
 get '/basic' do
-  @first = params[:first].to_f
-  @second = params[:second].to_f
-  @operator = params[:operator]
+  first = params[:first].to_f
+  second = params[:second].to_f
+  operator = params[:operator]
 
-  @result = case @operator
-            when "+" then @first + @second
-            when "-" then @first - @second
-            when "*" then @first * @second
-            when "/" then @first / @second
+  @result = case operator
+            when "+" then first + second
+            when "-" then first - second
+            when "*" then first * second
+            when "/" then first / second
             end
 
   erb :basic
@@ -23,15 +23,15 @@ end
 
 get '/advanced' do
 
-  @first = params[:first].to_f
-  @second = params[:second].to_f
-  @square = params[:square].to_f
+  first = params[:first].to_f
+  second = params[:second].to_f
+  square = params[:square].to_f
 
   if params[:first]
-    @result_power = @first**@second
-  elsif params[:square] && @square >= 0
-    @result_root = Math.sqrt(@square)
-  elsif params[:square] && @square < 0
+    @result_power = first**second
+  elsif params[:square] && square >= 0
+    @result_root = Math.sqrt(square)
+  elsif params[:square] && square < 0
     @result_root = "unable to determine the square root of a negative number"      
   else
     @result_power = nil
@@ -44,6 +44,18 @@ get '/advanced' do
 end
 
 get '/mortgage' do
+
+principal = params[:principal].to_f
+interest = params[:interest].to_f / 12 / 100
+number = params[:number].to_f
+
+interest_sub_calc = (1 + interest)**number
+numerator = interest * interest_sub_calc
+denominator = interest_sub_calc - 1
+@result = principal * (numerator / denominator)
+
+
+  erb :mortgage
 
 end
 
