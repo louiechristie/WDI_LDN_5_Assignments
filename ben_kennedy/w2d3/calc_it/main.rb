04cc95265bf10e2cb94d57576c6c0ca5d17f4c34
Ben Kennedy <bgkennedy@gmail.com>
2014-04-09 20:49:00 +0100

    completed homework, breaks under some NaN circumstances

--- ben_kennedy/w2d3/calc_it/main.rb
+++ ben_kennedy/w2d3/calc_it/main.rb
@@ -11,7 +11,9 @@ post '/navigate' do
   when 'basic' then redirect to('/basic')
   when 'advanced' then redirect to('/advanced')
   when 'trip' then redirect to('/trip')
-  # when 'celebrity' then redirect to('/celebrity')
+  when 'bmi' then redirect to('/bmi_choice')
+  when 'mortgage' then redirect to('/mortgage')
+  when 'personality' then redirect to('/personality')
   else redirect to('/')
   end
 end
@@ -59,9 +61,56 @@ get '/trip' do
   @time = (@distance.to_f / @speed).round(1)
   @cost = (@distance / @mpg * @price).round(0) if params[:mpg]
   erb :trip
-
 end
 
-# get '/celebrity' do
-#   erb :celebrity
-# end
+ get '/bmi_choice' do
+    case params[:system]
+    when 'imperial' then redirect to('/bmi_imperial')
+    when 'metric' then redirect to('/bmi_metric')
+    end
+    erb :bmi_choice
+  end
+
+get '/bmi_imperial' do
+
+  height = params[:height]
+  weight = params[:weight]
+  @bmi = (703 * weight.to_f / height.to_f ** 2).round(0) if height && weight
+
+  erb :bmi_imperial
+end
+
+get '/bmi_metric' do
+  
+  height = params[:height]
+  weight = params[:weight]
+  @bmi = (weight.to_f / height.to_f ** 2).round(0) if height && weight
+
+  erb :bmi_metric
+end
+
+get '/mortgage' do
+    
+  @principal = params[:principal]
+  @rate = params[:rate].to_f / 100 / 12
+  @years = params[:years]
+  payments = @years.to_i * 12
+  @monthly_payment = (@principal.to_f * ((@rate * (1 + @rate) ** payments)/((1 + @rate)**payments - 1))).round(2) if @years && @principal
+
+  erb :mortgage
+end
+   
+get '/personality' do
+  
+  @display_personality = true if params != {}
+  @who_you_are = [
+    "A big fat jerk",
+    "A kind and loving soul",
+    "A person to bring home to your mother",
+    "Good lord, words cannot describe",
+    "I could take you or leave you",
+    "You're not worth the bits required to articulate a description",
+    "I LOVE you!!!!!!"].shuffle![0]
+
+  erb :personality
+end
