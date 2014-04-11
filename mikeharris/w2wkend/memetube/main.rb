c98fe95e710d0072b6fb3e6735e6106a1fb7d92b
mikemjharris <mike.mj.harris@gmail.com>
2014-04-11 21:56:29 +0100

    create sql file and database for memetube homework

---
+++ mikeharris/w2wkend/memetube/main.rb
@@ -0,0 +1,84 @@
+require 'sinatra'
+require 'pry'
+require 'sinatra/reloader' if development?
+
+require 'pg'
+
+before do
+      @db = PG.connect(:dbname =>'memetube', :host => 'localhost')
+end
+
+after do 
+    @db.close
+end
+
+
+get '/'  do
+    redirect '/show'
+end
+
+get '/new'  do
+ erb :new 
+end
+
+post '/create'  do
+    @title = params[:title]
+    @todo = params[:todo]
+    
+    sql = "insert into todo (title, todo) values('#{@title}', '#{@todo}')"
+    @db.exec(sql)
+    
+ redirect '/show'
+end
+
+
+get '/show' do
+  sql="select * from todo"
+  @todolist = @db.exec(sql)
+
+  erb :show
+end
+
+get '/delete/:id'  do 
+     @id = params[:id]
+     sql = "delete FROM todo where id = '#{@id}'"
+     @db.exec(sql)
+    
+      redirect to '/show'
+
+end
+
+get '/edit/:id'  do 
+
+     @id = params[:id]
+     sql = "select * from todo"
+     # change to sql to extract from database.  doh
+     @todolist = @db.exec(sql)
+
+     @todolist.each do |item|
+        if item["id"] == @id
+              @entry_to_edit = item
+        end
+     end 
+
+      
+   erb  :edit
+
+end
+
+post '/update/:id' do
+   @id = params[:id]
+   @title = params[:title]
+   @todo = params[:todo]
+
+    sql = "update todo SET title='#{@title}', todo='#{@todo}' where id = '#{@id}'"
+    @db.exec(sql)
+
+redirect to '/'
+
+
+ end
+
+
+
+
