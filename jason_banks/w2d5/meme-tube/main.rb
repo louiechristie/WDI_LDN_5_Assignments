d48c8ec4455d6c97c17fea73dbfb41d3b8ed200d
jasonklein <jasonklein@gmail.com>
2014-04-12 21:59:07 +0100

    Complete index view, start upload view.

--- jason_banks/w2d5/meme-tube/main.rb
+++ jason_banks/w2d5/meme-tube/main.rb
@@ -1,4 +1,41 @@
 require "pry"
 require "sinatra"
 require "sinatra/reloader" if development?
-require ""
+require "pg"
+
+before do
+
+  @db = PG.connect(dbname: 'meme_tube_app', host: 'localhost')
+
+end
+
+after do
+
+  @db.close
+
+end
+
+get '/' do
+
+  featured_sql = "select id from videos"
+  featured_video = @db.exec(featured_sql).to_a.sample
+  
+erb :index
+
+end
+
+get '/upload' do
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
