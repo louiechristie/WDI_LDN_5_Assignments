710f6b8932a72bc7f2baed8b69bf738f6eac373e
ScottyGams <ScottyGammon@gmail.com>
2014-04-14 00:46:27 +0100

    FInally got videos to play, learning lessons from TODO to make my site more RESTful

--- ScottyGammon/2Week/Endw2/MemeTube/main.rb
+++ ScottyGammon/2Week/Endw2/MemeTube/main.rb
@@ -11,15 +11,14 @@ after do
 	@db.close
 end
 
+=begin
 get '/' do 
 	@youtube_id = params[:youtube_id]
 	@title = params[:title]
 	@description = params[:description]
 	@genre = params[:genre]
 	if @youtube_id && @youtube_id > ""
-		sql= "insert into videos (youtube_id, title, description, genre) values ('#{@youtube_id}', '#{@title}', '#{@description}', '#{@genre}') "
-
-		@db.exec(sql)
+		sql= "insert into videos (youtube_id, title, description, genre) values ('#{@youtube_id}', '#{@title}', '#{@description}', '#{@genre}') "		@db.exec(sql)
 	end
 
 	erb :video
@@ -30,4 +29,55 @@ get '/videos' do
 	@videos = @db.exec(sql)
 	
 	erb :video	
+=end
+
+
+get '/'  do
+    erb :Navigation
 end
+
+get '/show' do
+    sql="select * from videos order by title"
+
+  @videos = @db.exec(sql)
+
+  erb :show
+
+end
+
+get '/new'  do
+ erb :new 
+end
+
+post '/create'  do
+    @title = params[:title]
+    @description = params[:description]
+    @youtube_id = params[:youtube_id]
+    @genre = params[:genre]
+
+    sql = "insert into videos (title, description, youtube_id, genre) values('#{@title}', '#{@description}', '#{@youtube_id}', '#{@genre}')"
+    @db.exec(sql)
+    
+ redirect '/show'
+end
+
+
+get '/delete/:id'  do 
+     @id = params[:id]
+     sql = "delete FROM videos where id = '#{@id}'"
+     @db.exec(sql)
+    
+    redirect to '/show'
+
+end
+
+get '/edit/:id'  do 
+
+     @id = params[:id]
+     sql = "select * from videos where id='#{@id}'"
+     @videos = @db.exec(sql)[0]
+           
+   erb  :edit
+end
+
+
