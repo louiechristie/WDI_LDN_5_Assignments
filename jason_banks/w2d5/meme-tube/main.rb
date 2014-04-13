ec9cb386585e5f9d3cdc31a27b5a47f277d64b34
jasonklein <jasonklein@gmail.com>
2014-04-13 12:19:44 +0100

    Adjust some views and styling.

--- jason_banks/w2d5/meme-tube/main.rb
+++ jason_banks/w2d5/meme-tube/main.rb
@@ -35,16 +35,16 @@ post '/create' do
   id = params[:id]
   keyword = params[:keyword]
   confirm_sql = "SELECT * FROM videos WHERE id = '#{sql_string(id)}'"
-  @confirm_video = @db.exec(confirm_sql).to_a
+  @confirm_video = @db.exec(confirm_sql).first
+  binding.pry
 
-  if !@confirm_video
+  if @confirm_video
+    redirect to("/add")
+  else
     add_sql = "INSERT INTO videos (id, title, keyword) VALUES ('#{sql_string(id)}', '#{sql_string(title)}', '#{sql_string(keyword)}')"
     @db.exec(add_sql)
-  end
-
-  redirect to("/show/#{id}")
-
-  
+    redirect to("/show/#{sql_string(id)}")
+  end 
 end
 
 get '/show/:id' do
@@ -53,6 +53,24 @@ get '/show/:id' do
   @show_video = @db.exec(show_video_sql).first
 
   erb :show
+
+end
+
+get '/viewall' do
+
+  videos_sql = "SELECT * FROM videos ORDER BY title"
+  @all_videos = @db.exec(videos_sql)
+
+  erb :viewall
+
+end
+
+get '/keywords' do
+
+  keywords_sql = "SELECT keyword FROM videos"
+  @all_keywords = @db.exec(keywords_sql)
+
+  erb :keywords
 
 end
 
