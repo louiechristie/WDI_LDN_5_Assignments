567416922ae4f6475b9f9027b0a819c2a603b613
mikemjharris <mike.mj.harris@gmail.com>
2014-04-13 21:31:16 +0100

    added filter option - slowly updating css.

--- mikeharris/w2wkend/memetube/main.rb
+++ mikeharris/w2wkend/memetube/main.rb
@@ -17,7 +17,8 @@ get '/'  do
 end
 
 get '/show' do 
-  
+  sql = ("select genre from videos group by genre order by genre")
+  @genre_list = @db.exec(sql)
   if params[:filter] 
     sql="select * from videos WHERE genre='#{params[:filter]}'"
   else
@@ -27,15 +28,24 @@ get '/show' do
   erb :show
 end
 
+get '/show/:id' do
+  @id = params[:id]
+
+  sql = "select url from videos where id='#{@id}'"
+  @url = @db.exec(sql)[0]["url"]
+
+  erb :playclip 
+end
+
 get '/new'  do
  erb :new 
 end
 
 post '/create'  do
-    @title = params[:title]
+    @title = params[:title].capitalize
     @description = params[:description]
     @url = params[:url]
-    @genre = params[:genre]
+    @genre = params[:genre].capitalize
 
     sql = "insert into videos (title, description, url, genre) values('#{@title}', '#{@description}', '#{@url}', '#{@genre}')"
     @db.exec(sql)
@@ -67,10 +77,10 @@ end
 
 post '/update/:id' do
     @id = params[:id]
-    @title = params[:title]
+    @title = params[:title].capitalize
     @description = params[:description]
     @url = params[:url]
-    @genre = params[:genre]
+    @genre = params[:genre].capitalize
 
     
     sql = "update videos SET title='#{@title}', description='#{@description}', url='#{@url}', genre ='#{@genre}' where id = '#{@id}'"
