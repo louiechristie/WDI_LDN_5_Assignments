2fa946142689f1b010cc830bb5c1c29513b69970
jasonklein <jasonklein@gmail.com>
2014-04-13 02:47:16 +0100

    Complete upload and show views.

--- jason_banks/w2d5/meme-tube/main.rb
+++ jason_banks/w2d5/meme-tube/main.rb
@@ -16,16 +16,53 @@ after do
 end
 
 get '/' do
+  featured_sql = "SELECT * FROM videos"
+  @featured_video = @db.exec(featured_sql).to_a.sample
 
-  featured_sql = "select id from videos"
-  featured_video = @db.exec(featured_sql).to_a.sample
-  
-erb :index
+  erb :index
 
 end
 
-get '/upload' do
+get '/add' do
+
+  erb :add
+
+end
+
+post '/create' do
+
+  binding.pry
+  title = params[:title]
+  id = params[:id]
+  keyword = params[:keyword]
+  binding.pry
+  confirm_sql = "SELECT * FROM videos WHERE id = '#{sql_string(id)}'"
+  binding.pry
+  @confirm_video = @db.exec(confirm_sql).to_a
+
+  if !@confirm_video
+    add_sql = "INSERT INTO videos (id, title, keyword) VALUES ('#{sql_string(id)}', '#{sql_string(title)}', '#{sql_string(keyword)}')"
+    @db.exec(add_sql)
+  end
+
+  redirect to("/show/#{id}")
+
   
+end
+
+get '/show/:id' do
+  binding.pry
+  id = params[:id]
+  show_video_sql = "SELECT * FROM videos where id = '#{sql_string(id)}'"
+  @show_video = @db.exec(show_video_sql).first
+
+  erb :show
+
+end
+
+def sql_string(value)
+  value.gsub("'", "''")
+end
 
 
 
