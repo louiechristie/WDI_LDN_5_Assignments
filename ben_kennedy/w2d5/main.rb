9a8ee0548d3e1041c82205a3ccd0ce4458dc7431
Ben Kennedy <bgkennedy@gmail.com>
2014-04-14 09:23:38 +0100

    functional, but ugly homework

--- ben_kennedy/w2d5/main.rb
+++ ben_kennedy/w2d5/main.rb
@@ -5,11 +5,28 @@ require 'pg'
 
 before do
   @db = PG.connect(dbname: 'video_app', host: 'localhost')  
+  menu_select = "select tags from videos"
+  db_tags = @db.exec(menu_select).to_a
+  
+  @menu_tags = ""
+  @menu_tags_final = { }
+  db_tags.each { |tag_hash|
+    @menu_tags << tag_hash["tags"].gsub("{", "").gsub("}",",")
+  }
+  @menu_tags.lines(",").uniq { |tag|
+    @menu_tags_final[tag] = @menu_tags.lines(",").count(tag)
+  }
+  @menu = [ ] 
+  3.times do
+    @menu << @menu_tags_final.key(@menu_tags_final.values.max).gsub(",", "")
+    @menu_tags_final.delete(@menu_tags_final.key(@menu_tags_final.values.max))
+  end          
 end
 
 after do
   @db.close
 end
+
 
 get '/'  do 
 
@@ -21,6 +38,13 @@ get '/index'  do
     sql_select = "select id, video_name, created_at, tags from videos"
     @video_list = @db.exec(sql_select).to_a
    erb :index 
+end
+
+get '/index/:tag' do
+  @index = params["tag"]
+    sql_select = "select id, video_name, created_at, tags from videos where tags @> '{#{@index}}'"
+    @video_list = @db.exec(sql_select).to_a
+  erb :index
 end
   
 get '/new' do 
@@ -38,11 +62,13 @@ post '/create' do
     sql_add = "insert into videos (video_name, video_description, video_id, tags, created_at, posted_by) values ('#{@video_name}', '#{@video_description}', '#{@video_id}', '#{@tags}', current_timestamp, '#{@posted_by}');"
     @db.exec(sql_add)
   end
+    sql_select = "select id, video_name, created_at, tags from videos where tags @> '{#{@index}}'"
+    @video_list = @db.exec(sql_select).to_a
    erb :index
 end
 
 get '/show/:id' do 
-  @id = @env["PATH_INFO"].chars.pop
+  @id = @env["PATH_INFO"].lines(':').pop
   sql_select = "select * from videos where id = '#{@id}'"
   @video = @db.exec(sql_select).to_a[0]
 erb :show
@@ -62,7 +88,7 @@ post '/update/:id' do
   @tags = "{"+ params[:tags] + "}"
   @posted_by = params[:posted_by]
   if @video_name && @video_description && @video_id && @posted_by
-    sql_update = "insert into videos (video_name, video_description, video_id, tags, updated_at, posted_by) values ('#{@video_name}', '#{@video_description}', '#{@video_id}', '#{@tags}', current_timestamp, '#{@posted_by}');"
+    sql_update = "update videos set video_name = '#{@video_name}', video_description = '#{@video_description}', video_id = '#{@video_id}', tags = '#{@tags}', updated_at = current_timestamp, posted_by ='#{@posted_by}' where id = '#{params[:id].gsub(":", "")}';"
     @db.exec(sql_update)
   end
   sql_select = "select * from videos where id = '#{params[:id].gsub(":", "")}'"
@@ -71,10 +97,10 @@ post '/update/:id' do
 end
 
 get '/delete/:id' do 
-  # @delete = params[:delete]
-  # delete_sql = "delete from list where id = '#{@delete}'"
-  # @db.exec(delete_sql)
-  # sql_select = "select * from list"
-  # @task_list = @db.exec(sql_select).to_a  
-  # erb :home
+  @delete = params[:id].gsub(":", "")
+  delete_sql = "delete from videos where id = '#{@delete}'"
+  @db.exec(delete_sql)
+    sql_select = "select id, video_name, created_at, tags from videos"
+  @video_list = @db.exec(sql_select).to_a
+  erb :index
 end
