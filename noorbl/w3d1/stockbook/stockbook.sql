d46e3c99fbe95ba31d873ec1ec35f132db289d80
NoorBL <noorbinladin@gmail.com>
2014-04-14 16:23:11 +0100

    stockbook

---
+++ noorbl/w3d1/stockbook/stockbook.sql
@@ -0,0 +1,12 @@
+
+create table categories (
+id serial4 primary key,
+name varchar(255) not null
+);
+
+create table portfolios (
+id serial4 primary key,
+name varchar(255) not null,
+description text, 
+category_id integer
+);
