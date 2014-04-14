5e434a7dc78a901897e044f6d137fa8988a12de2
NirajVyas <nirajvyas28@hotmail.com>
2014-04-14 22:47:42 +0100

    all working no style

--- Niraj/w3d1/stockbook/controllers/stocks_controller.rb
+++ Niraj/w3d1/stockbook/controllers/stocks_controller.rb
@@ -1,30 +1,34 @@
 get '/stocks' do
-@stocks = Stock.all
-erb :'stocks/index'
+  @stocks = Stock.all
+  erb :'stocks/index'
 end
 
 get '/stocks/new' do
-@stock = Stock.new
+  @stock = Stock.new
   @portfolios = Portfolio.all
-erb :'stocks/new'
+  erb :'stocks/new'
 end
 
 post '/stocks' do
   @stock = Stock.new(params[:stock])
+  @update_details_yahoo
   @stock.save
 
   redirect to('/stocks')
 end
 
 get '/stocks/:id/edit' do
-@stock = Stock.find(params[:id])
+  @stock = Stock.find(params[:id])
   @portfolios = Category.all
-erb :'stocks/edit'
+  erb :'stocks/edit'
 end
 
 post '/stocks/:id' do
   @stock = Stock.find(params[:id])
   @stock.update_attributes(params[:stock])
+  @stock.update_details_yahoo
+  @stock.save
+  binding.pry
   redirect to("/stocks")
 end
 
--- Niraj/w3d1/stockbook/models/stock.rb
+++ Niraj/w3d1/stockbook/models/stock.rb
@@ -1,28 +1,87 @@
+# class Stock < DBBase
+
+
+# attributes symbol: :string,
+#      name: :string,
+#      buying_price: :decimal,
+#      holding: :integer,
+#      portfolio_id: :integer
+
+#     def self.table_name
+#       :stocks
+#     end
+
+#     def initialize(params={})
+#       @id = params['id']
+#       @symbol = params['symbol']
+#       @buying_price = params['buying_price']
+#       @holding = params['holding']
+#       @portfolio_id = params['portfolio_id']
+      
+#     end
+
+#     def update_details_yahoo
+#       stock_quote
+#     end
+
+#       def portfolio
+#     Portfolio.find(portfolio_id)
+#   end
+
+# private
+#   def stock_quote
+
+#     if self.symbol && self.symbol > ""
+#         yahoo_result = YahooFinance::get_standard_quotes(self.symbol)[self.symbol]
+#       @buying_price ||= yahoo_result.lastTrade
+#       @name = yahoo_result.name
+#     end
+#   end
+# end
+
 class Stock < DBBase
 
+  attributes symbol: :string, name: :string, buying_price: :decimal, holding: :integer, portfolio_id: :integer
+  attr_reader :value
 
-attributes symbol: :string,
-     name: :string,
-     buying_price: :decimal,
-     holding: :integer,
-     portfolio_id: :integer
+  def self.table_name
+    :stocks
+  end
 
-    def self.table_name
-      :stocks
-    end
+  def initialize(params={})
+    @id = params['id']
+    @symbol = params['symbol'].to_s.upcase
+    @name = params['name']
+    @buying_price = params['buying_price']
+    @holding = params['holding']
+    @portfolio_id = params['portfolio_id']
+    
+  end
 
-    def initialize(params={})
-      @id = params['id']
-      @symbol = params['symbol']
-      @buying_price = params['buying_price']
-      @holding = params['holding']
-      @portfolio_id = params['portfolio_id']
-    end
-
-      def portfolio
+  def portfolio
     Portfolio.find(portfolio_id)
+  end
+
+    def update_details_yahoo
+     stock_quote
+     end
+
+  private
+  def stock_quote
+    if self.symbol && self.symbol > ""
+      yahoo_result = YahooFinance::get_standard_quotes(self.symbol)[self.symbol]
+      @buying_price ||= yahoo_result.lastTrade
+      @name = yahoo_result.name
+    end
+  end
+
+  def quote_value
+    if self.symbol && self.symbol > ""
+    yahoo_result = YahooFinance::get_standard_quotes(self.symbol)[self.symbol]
+    price = yahoo_result.lastTrade
+    @value = price * @holding
+    end
   end
 
 
 end
-
