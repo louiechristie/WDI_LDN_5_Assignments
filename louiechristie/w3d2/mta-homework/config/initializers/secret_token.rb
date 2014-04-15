5c5c45b609888c8fc8bb7d3a7405e8ea2a5d64b1
louiechristie <post@louiechristie.com>
2014-04-15 22:17:24 +0100

    MTA homework. No error checking

---
+++ louiechristie/w3d2/mta-homework/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanzine::Application.config.secret_token = '0eb37b985d1712ea0401d8582018d066251b8934a720acf4b0357113ec270daa4ded3fd1d56b90eeb113b058fed47e9b57cb3ba227d86181edcbc940d22acc92'
