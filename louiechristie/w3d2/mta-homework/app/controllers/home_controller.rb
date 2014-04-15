8272620c47c5dc670b012c98185ac5eafec28688
louiechristie <post@louiechristie.com>
2014-04-15 23:05:50 +0100

    title updated, list subway names

--- louiechristie/w3d2/mta-homework/app/controllers/home_controller.rb
+++ louiechristie/w3d2/mta-homework/app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
 
   def index
 
-  n = [:nts, :n34, :n28, :n23, :us, :n8]
-  l = [:l8, :l6, :us, :l3, :l1]
-  s = [:sgc, :s33, :s28, :s23, :us, :sap]
-  subway = { n: n, l: l, s: s}
+  @n = [:nts, :n34, :n28, :n23, :us, :n8]
+  @l = [:l8, :l6, :us, :l3, :l1]
+  @s = [:sgc, :s33, :s28, :s23, :us, :sap]
+  @subway = { n: @n, l: @l, s: @s}
 
   if params[:start_line] && params[:start_line] > "" && params[:start_point] && params[:start_point] > "" && params[:end_line] && params[:end_line] > "" && params[:end_point] && params[:end_point] > ""
 
@@ -18,18 +18,18 @@ class HomeController < ApplicationController
 
     # calculate the distance if they are both on the same end_line
     if start_line == end_line
-      length = (subway[start_line].index(start_point) - subway[start_line].index(end_point)).abs
+      length = (@subway[start_line].index(start_point) - @subway[start_line].index(end_point)).abs
 
     # calculate the distance if they are on different lines
     else
       #get intersection
-      intersection = (subway[start_line] & subway[end_line])[0]
+      intersection = (@subway[start_line] & @subway[end_line])[0]
 
       #get distance from start point to intersection
-      start_line_distance = (subway[start_line].index(start_point) - subway[start_line].index(intersection)).abs
+      start_line_distance = (@subway[start_line].index(start_point) - @subway[start_line].index(intersection)).abs
 
       #get distance from end point to intersection
-      end_line_distance = (subway[end_line].index(end_point) - subway[end_line].index(intersection)).abs
+      end_line_distance = (@subway[end_line].index(end_point) - @subway[end_line].index(intersection)).abs
 
       #add distances together
       length = start_line_distance + end_line_distance
