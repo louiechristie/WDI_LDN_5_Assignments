23492ec858a940d513a69a630b61ca50a84ffdf8
louiechristie <post@louiechristie.com>
2014-04-16 21:20:38 +0100

    edit, update

--- louiechristie/w3d3/cars_app/app/controllers/cars_controller.rb
+++ louiechristie/w3d3/cars_app/app/controllers/cars_controller.rb
@@ -10,6 +10,16 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update_attributes(params[:car])
+    redirect_to(cars_path)  
+  end
+
   def create
    Car.create(params["car"])
    redirect_to(cars_path)
