ca47204150c8030d0f22354a686ecd016b9372b9
mikemjharris <mike.mj.harris@gmail.com>
2014-04-16 17:05:23 +0100

    created file structure and rails app for cars

---
+++ mikeharris/w3d3/cars_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarsApp::Application.config.secret_token = '0bbd2b5c7a25ff038e83030d5ac8f6b8a43486cf8526e7ffce795b1935b940dce31fe3a9c3a2f757a73236c4804b1dbd43384575eb0c6333a0ba5954e0a35189'
