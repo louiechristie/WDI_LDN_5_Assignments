a87f729e8935f02fb71fcb23fba8ce5b5237b6b1
louiechristie <post@louiechristie.com>
2014-04-16 18:43:52 +0100

    Planets lab and updates to homework from earlier in the week

--- louiechristie/w3d1/stockbook/models/stock.rb
+++ louiechristie/w3d1/stockbook/models/stock.rb
@@ -15,11 +15,22 @@ class Stock < DBBase
     @symbol = params['symbol']
     @name = params['name']
     @buying_price = params['buying_price']
+    @holding = params['holding']
     @portfolio_id = params['portfolio_id']
+    stock_quote
   end
 
   def portfolio
     Portfolio.find(portfolio_id)
+  end
+
+  private
+  def stock_quote
+    if self.symbol && self.symbol > ""
+      yahoo_result = YahooFinance::get_standard_quotes(self.symbol.upcase)[self.symbol.upcase]
+      @buying_price ||= yahoo_result.lastTrade
+      @name = yahoo_result.name
+    end
   end
 
 end
