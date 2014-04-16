5eb9fd5c3316967c08ed04143009658012505776
ScottyGams <ScottyGammon@gmail.com>
2014-04-16 09:35:27 +0100

    Adding my Week 3 Day 3 folder, Fanzine index and non-functional MTA on Rails

---
+++ ScottyGammon/3Week/2Dw3/MTA_Lab/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MTALab::Application.config.secret_token = 'ecac19f1b002f74ef6e1263607e081f934977f0d747e1e92d6d1bc74468c8e14b07323fd9689136abd8b747b86358b48297c0e894dc18836e57b099f14fc34d5'
