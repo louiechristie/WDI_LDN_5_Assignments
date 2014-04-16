cdda0e09ff3d6e8c4e5f58d1591e3033438a33d6
jasonklein <jasonklein@gmail.com>
2014-04-16 22:18:02 +0100

    Complete index view with functional linking.

--- jason_banks/w3d3/vehicles_app/app/controllers/vehicles_controller.rb
+++ jason_banks/w3d3/vehicles_app/app/controllers/vehicles_controller.rb
@@ -1,5 +1,10 @@
 class VehiclesController < ApplicationController
 
+  def index
+    @vehicles = Vehicle.order('country')
+
+  end
+
   def new
     @vehicle = Vehicle.new
     @type_of_vehicle_array = ['Airplane', 'Jet', 'Helicopter', 'Jeep', 'Truck', 'Car', 'Train', 'Coach (motor)', 'Coach (non-motor)', 'Tank/heavily-armored vehicle']
@@ -7,13 +12,26 @@ class VehiclesController < ApplicationController
 
   def create
     @vehicle = Vehicle.create(params[:vehicle])
-    @vehicle.inspect
+
     redirect_to(vehicle_path(@vehicle))
   end
 
   def show
     @vehicle = Vehicle.find(params[:id])
   end
+
+  def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def destroy
+
+    Vehicle.destroy(params[:id])
+
+    redirect_to(vehicles_path)
+
+  end
+
 
 
 
