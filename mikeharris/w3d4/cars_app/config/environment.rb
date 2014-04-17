7f889dda80f42279e4b07753dda65b351f228a51
mikemjharris <mike.mj.harris@gmail.com>
2014-04-17 19:01:40 +0100

    cars app homework done

---
+++ mikeharris/w3d4/cars_app/config/environment.rb
@@ -0,0 +1,12 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+CarsApp::Application.initialize!
+
+
+ActionMailer::Base.delivery_method = :smtp
+
+
+
+
