7d712cb5bfc50191f9c4f319247e404c115760fd
paulvarga23 <paul_varga@me.com>
2014-04-18 18:48:45 +0100

    cookbook - categories done

---
+++ paul_varga/aw3d4/cookbook/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: 'Main')
+Category.create(name: 'Starter')
+Category.create(name: 'Dessert')
+Category.create(name: 'Breakfast')
+Category.create(name: 'Snack')
