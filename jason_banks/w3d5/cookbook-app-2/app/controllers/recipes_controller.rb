3f5f03cad9ce940e1f9595ca4699a1178dc97ca5
jasonklein <jasonklein@gmail.com>
2014-04-19 18:05:17 +0100

    Add index, new, edit, and form views, and corresponding actions for recipes. Everything functional.

--- jason_banks/w3d5/cookbook-app-2/app/controllers/recipes_controller.rb
+++ jason_banks/w3d5/cookbook-app-2/app/controllers/recipes_controller.rb
@@ -1,23 +1,47 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
+    find_categories
   end
 
   def create
+    recipe = Recipe.create(params[:recipe])
+    unless recipe.valid?
+      flash[:notice] = "Recipe not saved due to incomplete information."
+    end
+
+    redirect_to(recipes_path)
   end
 
   def show
+    find_recipe
   end
 
   def edit
+    find_recipe
+    find_categories
   end
 
   def update
+    find_recipe.update_attributes(params[:recipe])
+    redirect_to(recipes_path)
   end
 
   def destroy
+    Recipe.destroy(params[:id])
+    redirect_to(recipes_path)
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def find_categories
+    @categories = Category.find(:all, :select => "id, name")
   end
 
 end
--- jason_banks/w3d5/cookbook-app-2/app/models/category.rb
+++ jason_banks/w3d5/cookbook-app-2/app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :image, :name
   validates :name, presence: true
+  validates :name, uniqueness: true
 
-  has_many :recipes, dependent: destroy
+  has_many :recipes, dependent: :destroy
 end
