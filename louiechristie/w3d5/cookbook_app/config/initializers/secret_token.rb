de9c1be486d44c6b125d1387617b16af41c13f65
louiechristie <post@louiechristie.com>
2014-04-19 19:09:31 +0100

    cookbook_app, list categories

---
+++ louiechristie/w3d5/cookbook_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookbookApp::Application.config.secret_token = 'd3832fca0087f05c8e2b3e7cf5fa8c5fd1c973723170593e4f6036a729605355c3433b26b3d5296027326d01490e28e9400fe00da0ed8da22eef79eafbc067d3'
