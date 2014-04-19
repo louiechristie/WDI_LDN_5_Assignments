65793910826c4b06bcccbd69dd9b71ccd32d73af
louiechristie <post@louiechristie.com>
2014-04-19 21:40:58 +0100

    cookbook_app, edit categories

--- louiechristie/w3d5/cookbook_app/app/controllers/categories_controller.rb
+++ louiechristie/w3d5/cookbook_app/app/controllers/categories_controller.rb
@@ -22,4 +22,14 @@ class CategoriesController < ApplicationController
     redirect_to(categories_path)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update_attributes(params[:category])
+    redirect_to(categories_path)
+  end
+
 end
