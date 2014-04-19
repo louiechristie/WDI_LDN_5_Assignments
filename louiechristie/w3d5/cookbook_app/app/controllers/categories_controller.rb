106cc87122cc11a6031cafbbff0564f2ca1dedf7
louiechristie <post@louiechristie.com>
2014-04-19 21:08:23 +0100

    cookbook_app, add/delete categories

--- louiechristie/w3d5/cookbook_app/app/controllers/categories_controller.rb
+++ louiechristie/w3d5/cookbook_app/app/controllers/categories_controller.rb
@@ -4,4 +4,22 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+   Category.create(params["category"])
+   redirect_to(categories_path)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to(categories_path)
+  end
+
 end
