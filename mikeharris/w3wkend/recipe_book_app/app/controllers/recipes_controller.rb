8bd97680eae4c46ca0e9f04e7ee018ed08657512
mikemjharris <mike.mj.harris@gmail.com>
2014-04-19 19:29:01 +0100

    recipe MVC basic structue completed

--- mikeharris/w3wkend/recipe_book_app/app/controllers/recipes_controller.rb
+++ mikeharris/w3wkend/recipe_book_app/app/controllers/recipes_controller.rb
@@ -5,5 +5,32 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  
+  def show
+    @recipe = Recipe.find_by_id(params[:id])
+  end
+
+  def edit 
+    @recipe = Recipe.find_by_id(params[:id])
+  end
+
+  def update
+    Recipe.find_by_id(params["id"]).update_attributes(params["recipe"])
+    redirect_to recipes_path
+  end
+
+  def create
+    Recipe.create(params["recipe"])
+    redirect_to recipes_path
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def destroy
+    Recipe.find_by_id(params[:id]).destroy
+    redirect_to recipes_path
+  end
+
 end
+
--- mikeharris/w3wkend/recipe_book_app/app/models/recipe.rb
+++ mikeharris/w3wkend/recipe_book_app/app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :description, :feeds, :image, :name, :time, :category_id
+
+  belongs_to :category
 end
