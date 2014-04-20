d53cbd793cef804f925f1930191a5f0444d5b24c
paulvarga23 <paul_varga@me.com>
2014-04-20 20:59:21 +0100

    update join table working

--- paul_varga/aw3d4/cookbook/app/models/recipe.rb
+++ paul_varga/aw3d4/cookbook/app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :category_id, :description, :feeds_people, :name
+  attr_accessible :category_id, :description, :feeds_people, :name, :ingridient_id, :ingridient_ids
 
   belongs_to :category
+  has_and_belongs_to_many :ingridients
 end
