e51ddfb5cb78b627d2f1f470cd19890984bdcb8d
NirajVyas <nirajvyas28@hotmail.com>
2014-04-21 22:33:04 +0100

    cooking homework 21st april

--- Niraj/weekhm/cooking_app/app/models/ingredients_recipe.rb
+++ Niraj/weekhm/cooking_app/app/models/ingredients_recipe.rb
@@ -7,7 +7,6 @@ class IngredientsRecipe < ActiveRecord::Base
 
 
 
-  validates :name, presence: true
 
 
 
--- Niraj/weekhm/cooking_app/app/models/recipe.rb
+++ Niraj/weekhm/cooking_app/app/models/recipe.rb
@@ -2,9 +2,9 @@ class Recipe < ActiveRecord::Base
   attr_accessible :category_id, :name, :serving_size, :picture, :ingredient_id, :ingredient_ids
 
   belongs_to :category
+has_many :ingredients_recipes
 
-  has_many :ingredients
-  has_many :ingredients_recipes
+has_many :ingredients, through: :ingredients_recipes
 
 
   validates :name, presence: true
