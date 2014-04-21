0913d4cd21945bdd36fcf0f80961865b3abe2138
hansss <hanakim@college.harvard.edu>
2014-04-21 18:50:52 +0100

    Created joint model

---
+++ Hana/w4d1/foodblog2/app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+def index
+  @categories = Category.all
+  @ingredients = Ingredient.all
+end
+
+def show
+  @category = Category.find(params[:id])
+end
+
+def new
+  @category = Category.new
+end
+
+def create
+  Category.create(params[:category])
+  redirect_to(root_path)
+end
+
+def destroy
+  @category = Category.find(params[:id])
+  @category.destroy
+  redirect_to root_path
+end
+
+end
