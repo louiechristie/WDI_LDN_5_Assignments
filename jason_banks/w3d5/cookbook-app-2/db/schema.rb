38587ef51ee1f71d8e15c4ba18ec2757e486be09
jasonklein <jasonklein@gmail.com>
2014-04-22 22:51:18 +0100

    Add User model and users table.

--- jason_banks/w3d5/cookbook-app-2/db/schema.rb
+++ jason_banks/w3d5/cookbook-app-2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140422120715) do
+ActiveRecord::Schema.define(:version => 20140422214920) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",       :null => false
