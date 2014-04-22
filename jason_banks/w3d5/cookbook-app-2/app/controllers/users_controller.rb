92a08b8222bf4184d5a7ac26a44f54bfe15f3b19
jasonklein <jasonklein@gmail.com>
2014-04-23 00:01:45 +0100

    Add create user functionality and users index view.

--- jason_banks/w3d5/cookbook-app-2/app/controllers/users_controller.rb
+++ jason_banks/w3d5/cookbook-app-2/app/controllers/users_controller.rb
@@ -8,6 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to(root_path, notice: "You have been successfully added to disastronomia.")
+    else
+      render :new
+    end
   end
+
 end
