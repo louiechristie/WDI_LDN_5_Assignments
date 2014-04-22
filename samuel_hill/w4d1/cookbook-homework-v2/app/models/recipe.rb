4059ebc2df95fbddd7849e7a001f6cf0562ccf79
samuel-hill <samuel_hill@me.com>
2014-04-22 08:56:28 +0100

    built relationships, all working in the ocnsole and on the show page, could not get the nested form working to add quantities

---
+++ samuel_hill/w4d1/cookbook-homework-v2/app/models/recipe.rb
@@ -0,0 +1,17 @@
+ class Recipe < ActiveRecord::Base
+
+  attr_accessible :category_id, :feeds, :instructions, :name, :ingredient_ids
+
+  belongs_to :category	
+
+  has_many :ingredients, through: :ingredients_recipes
+  has_many :ingredients_recipes
+
+  accepts_nested_attributes_for :ingredients
+
+  validates :name, presence: true
+  validates :feeds, numericality: { greater_than: 0 }
+  validates :instructions, presence: true
+  validates :ingredient_ids, presence: true
+  
+end
