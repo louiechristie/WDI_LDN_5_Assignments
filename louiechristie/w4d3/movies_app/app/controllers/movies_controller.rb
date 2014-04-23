86a05994395210cd5a59b5b8e6c9e92a822e3142
louiechristie <post@louiechristie.com>
2014-04-23 19:03:04 +0100

    w4d3 movies_app new, edit movie

--- louiechristie/w4d3/movies_app/app/controllers/movies_controller.rb
+++ louiechristie/w4d3/movies_app/app/controllers/movies_controller.rb
@@ -8,4 +8,37 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+
+    if @movie.save
+      redirect_to @movie, notice: 'Category was successfully created.'
+    else
+      redirect_to new_movie_path, notice: 'Category was not created.'  
+    end
+  end
+
+  def destroy
+    Movie.find(params[:id]).destroy
+    redirect_to(movies_path)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update_attributes(params[:movie])
+    redirect_to @movie, notice: 'Movie was successfully updated.'
+    else
+      redirect_to(edit_movies_path)
+    end
+  end
+
 end
