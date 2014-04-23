4db5b673ff37694bb0b00d2e9488944c03a05749
jasonklein <jasonklein@gmail.com>
2014-04-23 18:42:35 +0100

    Add create and show functionality.

--- jason_banks/w4d3/wdimdb/app/controllers/movies_controller.rb
+++ jason_banks/w4d3/wdimdb/app/controllers/movies_controller.rb
@@ -8,9 +8,17 @@ class MoviesController < ApplicationController
   end
 
   def create
+    @movie = Movie.new(params[:movie])
+
+    if @movie.save
+      redirect_to movie_path(@movie), notice: '"#{@movie.title}" has been added to the WDImdb.'
+    else
+      render 'new'
+    end
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def edit
--- jason_banks/w4d3/wdimdb/app/models/movie.rb
+++ jason_banks/w4d3/wdimdb/app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
   attr_accessible :rating, :review, :title, :how_viewed
+
+  validates :title, :rating, :how_viewed, presence: true
 end
