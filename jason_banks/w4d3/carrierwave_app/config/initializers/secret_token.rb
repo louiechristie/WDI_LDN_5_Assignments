ba13193606f6fdde3f84941f612d7ef210371c27
jasonklein <jasonklein@gmail.com>
2014-04-23 15:19:34 +0100

    Add rmagick gem and versioning of files.

---
+++ jason_banks/w4d3/carrierwave_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveApp::Application.config.secret_token = '29798e201e4e9381319cf6181065a75a4b0d55bfabfd1acafaddb80d2bb87b5c71e91b3af9bb6794d48390d2cca59032bac53a6887cb4f8de89734d783d526a5'
