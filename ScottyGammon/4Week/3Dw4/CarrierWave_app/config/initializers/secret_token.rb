8389e9238f20ae36af7d7880baf1890c07e96564
ScottyGams <ScottyGammon@gmail.com>
2014-04-24 09:11:35 +0100

    functional homework that uses API

---
+++ ScottyGammon/4Week/3Dw4/CarrierWave_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierWaveApp::Application.config.secret_token = 'ca0479234dac07e792aa4a542d63dfeee547240e476150d03e05f637d03c3956fc98505a18df5a3b0a68102633ed347b39a3e20a4afed3af48fe4ecb1ab4e88b'
