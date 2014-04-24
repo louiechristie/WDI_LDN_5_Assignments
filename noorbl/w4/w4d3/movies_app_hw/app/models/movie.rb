1a8cd8708b433f97b3961e1bfd00cdcc38377184
NoorBL <noorbinladin@gmail.com>
2014-04-24 08:54:56 +0100

    trying out carrierwave

--- noorbl/w4/w4d3/movies_app_hw/app/models/movie.rb
+++ noorbl/w4/w4d3/movies_app_hw/app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :actor, :description, :director, :review, :title, :trailer_url, :category_id
+  attr_accessible :actor, :description, :director, :review, :title, :trailer_url, :category_id, :movie_image
+
+  mount_uploader :movie_image, MovieImageUploader
 
   belongs_to :category
 
--- noorbl/w4/w4d3/movies_app_hw/db/schema.rb
+++ noorbl/w4/w4d3/movies_app_hw/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140423193406) do
+ActiveRecord::Schema.define(:version => 20140423201101) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20140423193406) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "category_id"
+    t.string   "movie_image"
   end
 
 end
