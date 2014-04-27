8c2ca2eaa6d434f17ad240a886ec75cb9c9db638
jasonklein <jasonklein@gmail.com>
2014-04-27 13:31:51 +0100

    Change image from string to text, to work better with the SVG tags.

--- jason_banks/w3d5/cookbook-app-2/db/schema.rb
+++ jason_banks/w3d5/cookbook-app-2/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140426160452) do
+ActiveRecord::Schema.define(:version => 20140427122857) do
 
   create_table "categories", :force => true do |t|
-    t.string   "name",       :null => false
-    t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",                      :null => false
+    t.text     "image",      :limit => 255
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "ingredients", :force => true do |t|
