5db46ace3868862683f885880c9b2ba6c867a616
jasonklein <jasonklein@gmail.com>
2014-05-05 16:09:34 +0100

    Add divide and square root functions.

--- jason_banks/w6d1/js-calcit/main.js
+++ jason_banks/w6d1/js-calcit/main.js
@@ -27,3 +27,17 @@ function multiply() {
 
   alert ("The product of " + array[0] + " and " + array[1] + " is " + result + ".");
 }
+
+function divide() {
+  var array = getTwoInputs();
+  var result = array[0] / array[1];
+
+  alert ("The quotient of " + array[0] + " and " + array[1] + " is " + result + ".");
+}
+
+function squareRoot() {
+  var first = parseInt(prompt("Please enter number."));
+  var result = Math.sqrt(first);
+
+  alert ("The square of " + first + " is " + result + ".");
+}
