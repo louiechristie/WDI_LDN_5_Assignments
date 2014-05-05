f64091b64aceee30cecba7ef15d0ee589a61163c
jasonklein <jasonklein@gmail.com>
2014-05-05 16:29:34 +0100

    Add exception handling for each math function.

--- jason_banks/w6d1/js-calcit/main.js
+++ jason_banks/w6d1/js-calcit/main.js
@@ -7,37 +7,61 @@ function getTwoInputs() {
   return array;
 }
 
+var errorNAN = "Not all of the inputs were numbers. Please try again.";
+
 function add() {
   var array = getTwoInputs();
   var result = array[0] + array[1];
 
-  alert ("The sum of " + array[0] + " and " + array[1] + " is " + result + ".");
+  if (isNaN(result)) {
+    alert (errorNAN);
+  } else {
+    alert ("The sum of " + array[0] + " and " + array[1] + " is " + result + ".");
+  }
 }
 
 function subtract() {
   var array = getTwoInputs();
   var result = array[0] - array[1];
 
-  alert ("The difference between " + array[0] + " and " + array[1] + " is " + result + ".");
+   if (isNaN(result)) {
+    alert (errorNAN);
+  } else {
+    alert ("The difference between " + array[0] + " and " + array[1] + " is " + result + ".");
+  }
 }
 
 function multiply() {
   var array = getTwoInputs();
   var result = array[0] * array[1];
 
-  alert ("The product of " + array[0] + " and " + array[1] + " is " + result + ".");
+  if (isNaN(result)) {
+    alert (errorNAN);
+  } else {
+    alert ("The product of " + array[0] + " and " + array[1] + " is " + result + ".");
+  }
 }
 
 function divide() {
   var array = getTwoInputs();
   var result = array[0] / array[1];
 
-  alert ("The quotient of " + array[0] + " and " + array[1] + " is " + result + ".");
+  if (array[1] == 0) {
+    alert ("Cannot divide by 0.");
+  } else if (isNaN(result)) {
+    alert (errorNAN);
+  } else {
+    alert ("The quotient of " + array[0] + " and " + array[1] + " is " + result + ".");
+  }
 }
 
 function squareRoot() {
   var first = parseInt(prompt("Please enter number."));
   var result = Math.sqrt(first);
 
-  alert ("The square of " + first + " is " + result + ".");
+  if (first < 0) {
+    alert ("Cannot find the square of a number less than 0.")
+  } else {
+    alert ("The square of " + first + " is " + result + ".");
+  }
 }
