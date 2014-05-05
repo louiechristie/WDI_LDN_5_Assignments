0b7e52ee0efe82377722473ef018240ab720e151
NoorBL <noorbinladin@gmail.com>
2014-05-05 15:56:52 +0100

    add images to movies_app, add bob lab, add calcit_js

---
+++ noorbl/w6/w6d1/calc_it_js_lab/main.js
@@ -0,0 +1,45 @@
+
+
+
+function add() {
+  var A = parseInt(prompt("Please enter first number."));
+  var B = parseInt(prompt("Please enter second number."));
+  var result = A + B;
+  alert("Your result is:" + result);
+}
+
+
+
+function subtract() {
+  var A = parseInt(prompt("Please enter first number."));
+  var B = parseInt(prompt("Please enter second number."));
+  var result = A - B;
+  alert("Your result is:" + result);
+}
+
+
+
+function divide() {
+  var A = parseInt(prompt("Please enter first number."));
+  var B = parseInt(prompt("Please enter second number."));
+  var result = A / B;
+  alert("Your result is:" + result);
+}
+
+
+
+
+function multiply() {
+  var A = parseInt(prompt("Please enter first number."));
+  var B = parseInt(prompt("Please enter second number."));
+  var result = A * B;
+  alert("Your result is:" + result);
+}
+
+
+
+function square_root() {
+  var C = parseInt(prompt("Please enter a number."));
+  var result = Math.sqrt(C);
+  alert("Your result is:" + result);
+}
