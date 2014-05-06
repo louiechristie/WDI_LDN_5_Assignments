60f68268f3cefc4125fb80448951a5c0f0a46ed4
jasonklein <jasonklein@gmail.com>
2014-05-06 18:01:14 +0100

    Add current withdraw and deposit functions.

--- jason_banks/w6d2/ga-bank-homework/main.js
+++ jason_banks/w6d2/ga-bank-homework/main.js
@@ -3,6 +3,43 @@ var BankApp = {};
 BankApp.currentAmount = 0;
 BankApp.savingsAmount = 0;
 
+BankApp.displayCurrentAmount = function () {
+  $display_current.text(BankApp.currentAmount);
+}
+
+BankApp.getDisplayCurrent = function () {
+  $display_current = $("#display_current");
+}
+
+BankApp.clearCurrentInput = function () {
+  $display_current.val("");
+}
+
+BankApp.displaySavingsAmount = function () {
+  $display_savings.text(BankApp.savingsAmount);
+}
+
+BankApp.getCurrentInput = function () {
+  $current_input = parseFloat($("#current_input").val());
+}
+
+BankApp.reduceCurrentAmount = function () {
+  BankApp.getCurrentInput();
+  BankApp.getDisplayCurrent();
+  BankApp.currentAmount -= $current_input;
+
+  BankApp.displayCurrentAmount();
+  BankApp.clearCurrentInput();
+}
+
+BankApp.increaseCurrentAmount = function () {
+  BankApp.getCurrentInput();
+  BankApp.getDisplayCurrent();
+  BankApp.currentAmount += $current_input;
+
+  BankApp.displayCurrentAmount();
+  BankApp.clearCurrentInput();
+}
 
 
 
@@ -10,6 +47,24 @@ BankApp.savingsAmount = 0;
 
 
 
-BankApp.setup = function () {}
+
+BankApp.setup = function () {
+  $display_current = $("#display_current");
+  BankApp.displayCurrentAmount();
+
+  $display_savings = $("#display_savings");
+  BankApp.displaySavingsAmount();
+
+  $("#current_withdraw_button").click( function (ev) {
+    ev.preventDefault();
+    BankApp.reduceCurrentAmount();
+  });
+  
+$("#current_deposit_button").click( function (ev) {
+    ev.preventDefault();
+    BankApp.increaseCurrentAmount();
+  });
+  
+}
 
 $(document).ready(BankApp.setup);
