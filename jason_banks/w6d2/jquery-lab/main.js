631fed16ce3ddd4da77643b6018e4e419f7b8ada
jasonklein <jasonklein@gmail.com>
2014-05-06 15:50:07 +0100

    Add error checking to form.

--- jason_banks/w6d2/jquery-lab/main.js
+++ jason_banks/w6d2/jquery-lab/main.js
@@ -1,6 +1,25 @@
 var ToDoApp = {};
 
-ToDoApp.new_item = function () {
+ToDoApp.items_list = [];
+
+ToDoApp.add_item = function () {
+  var $new_item = $('#new_item');
+  var $list = $('#list');
+
+  if ($new_item.val()){
+    ToDoApp.items_list.push($new_item);
+  } else {
+    alert("Cannot add a blank item.");
+  }
+
+}
+
+// ToDoApp.list_items = function () {;}
+
+
+
+ToDoApp.setup = function () {
+  $('#add_button').click(ToDoApp.add_item);
 
 }
 
