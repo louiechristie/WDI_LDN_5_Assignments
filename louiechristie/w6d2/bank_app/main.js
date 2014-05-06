f7de2313259b117d2b2372bdd855c3b43ef4d7e8
louiechristie <post@louiechristie.com>
2014-05-06 20:55:45 +0100

    w6d2 Checking and saving account

--- louiechristie/w6d2/bank_app/main.js
+++ louiechristie/w6d2/bank_app/main.js
@@ -1,40 +1,87 @@
 var myApp = myApp || {};
 
-myApp.balance = function() {
+myApp.checkingBalance = function() {
   if (arguments.length == 0) {
     var balance = parseFloat($('#checking .amount').text());
-    console.log("balance() balance: "+balance);
     return balance;
   }
   if (arguments.length > 0) {
-    console.log("setBalance("+arguments[0]+")");
     $('#checking .amount').text(arguments[0])  
   }
 }
 
-myApp.deposit = function() {
+myApp.checkingDeposit = function() {
   var amount = parseFloat($('#checking .input').val());
-  var balance = myApp.balance();
-  console.log("deposit(): "+amount);
-  myApp.balance(balance+amount);
+  if (!isNaN(amount)) {
+    var balance = myApp.checkingBalance();
+    myApp.checkingBalance(balance+amount);
+  }
 }
 
-myApp.withdraw = function() {
+myApp.checkingWithdraw = function() {
   var amount = parseFloat($('#checking .input').val());
-  var balance = myApp.balance();
-  console.log("withdraw(): "+amount);
-  myApp.balance(balance-amount);
+  var balance = myApp.checkingBalance();
+  var combined_balance = myApp.checkingBalance() + myApp.savingBalance();
+  if (!isNaN(amount)) {
+    if (balance >= amount) {
+      myApp.checkingBalance(balance-amount);
+    }
+    else {
+      if (combined_balance >= amount) {
+        var amount_off_checking = balance;
+        var amount_off_savings = amount - balance;
+
+
+
+        myApp.savingBalance(myApp.savingBalance()-amount_off_savings)
+        myApp.checkingBalance(0);
+
+      }
+    }
+
+  }
+}
+
+myApp.savingBalance = function() {
+  if (arguments.length == 0) {
+    var balance = parseFloat($('#saving .amount').text());
+    return balance;
+  }
+  if (arguments.length > 0) {
+    $('#saving .amount').text(arguments[0])  
+  }
+}
+
+myApp.savingDeposit = function() {
+  var amount = parseFloat($('#saving .input').val());
+  if (!isNaN(amount)) {
+    var balance = myApp.savingBalance();
+    myApp.savingBalance(balance+amount);
+  }
+}
+
+myApp.savingWithdraw = function() {
+  var amount = parseFloat($('#saving .input').val());
+  var balance = myApp.savingBalance();
+  if (!isNaN(amount)) {
+    if (balance >= amount) {
+      myApp.savingBalance(balance-amount);
+    }
+  }
 }
 
 myApp.setup = function() {
-  console.log("setup()");
   $('.amount').text("0");
   $('.input').val("0");
 
   var $checking_deposit_button = $('#checking .deposit');
-  $checking_deposit_button.click(function () { myApp.deposit(); });
   var $checking_withdraw_button = $('#checking .withdraw');
-  $checking_withdraw_button.click(function () { myApp.withdraw(); });
+  var $saving_deposit_button = $('#saving .deposit');
+  var $saving_withdraw_button = $('#saving .withdraw');
+  $checking_deposit_button.click(function () { myApp.checkingDeposit(); });
+  $checking_withdraw_button.click(function () { myApp.checkingWithdraw(); });
+  $saving_deposit_button.click(function () { myApp.savingDeposit(); });
+  $saving_withdraw_button.click(function () { myApp.savingWithdraw(); });
 }
 
 $(myApp.setup);
