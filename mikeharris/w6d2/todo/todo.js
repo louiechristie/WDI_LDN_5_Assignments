9044d92c8c9f43a06a58fadc9a2d69a2182fd413
mikemjharris <mike.mj.harris@gmail.com>
2014-05-06 16:02:11 +0100

    basic todo list working

--- mikeharris/w6d2/todo/todo.js
+++ mikeharris/w6d2/todo/todo.js
@@ -1,4 +1,38 @@
-$(todoApp.setup)
 
 
+var todo = todo || {};
 
+todo.addToDo = function () {
+     $todoList = $('#todo_list');
+     $newItem = $('#new_item').val();
+     $itemToAdd = $('<li>' +$newItem + "<div class='up'>move up</div><div class='down'>move down</div></li>");
+     $itemToAdd.addClass('highlight');
+     $itemToAdd.on('click', todo.done)
+     $todoList.append($itemToAdd);
+     todo.setup;
+    }
+
+todo.done = function() {
+    $todoListItems = $(this);
+    $todoListItems.toggleClass('cross_off')
+}
+todo.down = function() {
+  event.stopPropagation();
+  $todoItem = $(this).parent();
+  $todoItem.insertAfter($todoItem.next());
+}
+todo.up = function() {
+  event.stopPropagation();
+  $todoItem = $(this).parent();
+  $todoItem.insertBefore($todoItem.prev());
+}
+
+todo.setup = function () {
+  $('#add_to_do').on('click', todo.addToDo);
+  $('#todo_list li').on('click', todo.done);
+  $('.down').on('click', todo.down);
+  $('.up').on('click', todo.up);
+}
+
+
+$(todo.setup)
