4c3339915d0048e3dd86386b3e46d6778017c90b
jasonklein <jasonklein@gmail.com>
2014-05-06 16:50:07 +0100

    Add checkboxes.

--- jason_banks/w6d2/jquery-lab/main.js
+++ jason_banks/w6d2/jquery-lab/main.js
@@ -4,21 +4,13 @@ ToDoApp.add_item = function () {
   var $new_item = $('#new_item');
 
   if ($new_item.val()){
-    $("#list_items").append("<li>" + $new_item.val() + "</li>");
+    $("#list_items").append("<li>" + $new_item.val() + "<input type='checkbox' name='checkbox' id='" + $new_item.val() + "' /></li>");
   } else {
     alert("Cannot add a blank item.");
   }
   $new_item.val("");
 }
 
-// ToDoApp.list_items = function () {
-//   var $list_items = $('#list_items');
-
-//   $.each(ToDoApp.itemsList, function(i, el) {
-//     var $el = $(el);
-//     $list_items.text($el);
-//   });
-// }
 
 
 
@@ -28,7 +20,6 @@ ToDoApp.setup = function () {
     ToDoApp.add_item();
   });
 
-  $('#list').mouseover(ToDoApp.list_items);
 }
 
 $(document).ready(ToDoApp.setup);
