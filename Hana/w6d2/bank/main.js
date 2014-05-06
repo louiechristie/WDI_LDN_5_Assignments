a10b560407005f844b5f46528659fb35942dcbad
hansss <hanakim@college.harvard.edu>
2014-05-06 19:55:43 +0100

    Checking for extra money in savings working

--- Hana/w6d2/bank/main.js
+++ Hana/w6d2/bank/main.js
@@ -1,74 +1,57 @@
 var myApp = myApp || {}
 
-var total = 0;
+
 var checkings = 0;
 var savings = 0;
 
-myApp.checkingsDeposit = function(){
- // var caption = $('#checkingsText');
- var input = $('#checkingsNewitem').val();
 
-total += parseFloat(input);
+myApp.checkingsDeposit = function(){
+ var input = $('#checkingsNewitem').val();
 checkings += parseFloat(input);
-// caption.text("$" + checkings.toFixed(2));  
-myApp.showBalances(checkings);
+myApp.showBalances(checkings,savings);
 }
 
 myApp.checkingsWithdraw = function(){
- // var caption = $('#checkingsText');
- var input = $('#checkingsNewitem').val();
+  var total = checkings + savings;
+  var input = $('#checkingsNewitem').val();
 
   if(input<=total&&input<=checkings){
-    total -= parseFloat(input);
     checkings -= parseFloat(input);
-    // caption.text("$" + checkings.toFixed(2)); 
-    myApp.showBalances(checkings,savings,total); 
+    myApp.showBalances(checkings,savings); 
   }
-  else if(input<=total&&input>=checkings){
+  else if(input<=total&&input>checkings){
     remainder = parseFloat(input) - checkings;
     checkings = 0;
-    total -= remainder;
-    // caption.text("$" + checkings.toFixed(2));
-    myApp.showBalances(checkings,savings,total); 
+    savings -= remainder;
+    myApp.showBalances(checkings,savings); 
   }
 
 
-// //show total
-// var caption = $('#total');
-// caption.text("$" + total.toFixed(2)); 
 }
 
 myApp.savingsDeposit = function(){
- // var caption = $('#savingsText');
  var input = $('#savingsNewitem').val();
 
 total += parseFloat(input);
 savings += parseFloat(input);
-// caption.text("$" + savings.toFixed(2));  
-myApp.showBalances(checkings,savings,total); 
+myApp.showBalances(checkings,savings); 
 
 
-// //show total
-// var caption = $('#total');
-// caption.text("$" + total.toFixed(2)); 
 }
 
 
 myApp.savingsWithdraw = function(){
- // var caption = $('#savingsText');
- var input = $('#savingsNewitem').val();
+var total = checkings + savings;
+var input = $('#savingsNewitem').val();
 
-total -= parseFloat(input);
-savings -= parseFloat(input);
-// caption.text("$" + savings.toFixed(2));  
-myApp.showBalances(checkings,savings,total); 
-
-//show total
-// var caption = $('#total');
-// caption.text("$" + total.toFixed(2)); 
+  if(input<=savings){
+    savings -= parseFloat(input); 
+    myApp.showBalances(checkings,savings); 
+  }
 }
 
-myApp.showBalances = function(checkings, savings, total){
+myApp.showBalances = function(checkings, savings){
+  var total = checkings + savings;
   var checkingsCaption = $('#checkingsText');
   checkingsCaption.text("$" + checkings.toFixed(2));
   var savingsCaption = $('#savingsText');
@@ -99,14 +82,6 @@ $(myApp.setup);
 
 
 
-
-
-
-// ["savings, "checkings"].each { |type|
-//   $("button." + type).click(
-//     $("input." + type).val()
-//   )
-// }
 
 
 
