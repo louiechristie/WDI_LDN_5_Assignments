90ed57832b167a1e10b9ccd0f15b21f78d8df522
paulvarga23 <paul_varga@me.com>
2014-05-06 12:04:00 +0100

    update js unobtrusive

--- paul_varga/aw6d1/calc_js/main.js
+++ paul_varga/aw6d1/calc_js/main.js
@@ -1,4 +1,6 @@
-function calc() {
+var myApp = myApp || {};
+
+myApp.calc = function() {
   var a = parseFloat(document.getElementById('first_number').value);
   var b = parseFloat(document.getElementById('second_number').value);
   var caption = document.getElementById('result');
@@ -15,88 +17,26 @@ function calc() {
         break;
       case 'multiply':
         var result = (a * b).toFixed(2);
+        break;
       case 'divide':
         var result = (a / b).toFixed(2);
+        break;
       case 'squareRoot':
         var result = (Math.sqrt(a)).toFixed(2);
+        break;
       default: 
         break; 
     }
   caption.innerHTML = "Your result is: " + result + "!";
 }
 
-function mortgage() {
-  var principal = parseFloat(document.getElementById('principal').value);
-  var yearlyInterestRate = parseFloat(document.getElementById('yearlyInterestRate').value) / 1200;
-  var numberOfYears = parseFloat(document.getElementById('numberOfYears').value) * 12;
-
-  var caption = document.getElementById('repayment');
-
-  var repayment = (principal * ((yearlyInterestRate * (Math.pow((1 + yearlyInterestRate), numberOfYears))) / ((Math.pow((1 + yearlyInterestRate), numberOfYears))-1))).toFixed(2) 
-
-  caption.innerHTML = "Your repayment is: " + repayment + " per month!";
-}
-
-function bmiMetric() {
-  var weight = parseFloat(document.getElementById('weight').value);
-  var height = parseFloat(document.getElementById('height').value);
-  console.log(weight, height)
-  var caption = document.getElementById('bmiM');
-
-  var bmiM = ((weight / Math.pow(height, 2))).toFixed(2); 
-
-  caption.innerHTML = "Your BMI is: " + bmiM + "!";
-}
-
-function bmiImperial() {
-  var weight = parseFloat(document.getElementById('weight2').value);
-  var height = parseFloat(document.getElementById('height2').value);
-
-  var caption = document.getElementById('bmiI');
-
-  var bmiI = ((weight / Math.pow(height, 2))*703).toFixed(2); 
-
-  caption.innerHTML = "Your BMI is: " + bmiI + "!";
-}
-
-function trip() {
-  var distance = parseFloat(document.getElementById('distance').value);
-  var fuelEfficiency = parseFloat(document.getElementById('fuelEfficiency').value);
-  var costPerGallon = parseFloat(document.getElementById('costPerGallon').value);
-  var speed = parseFloat(document.getElementById('speed').value);
-
-  if (speed > 60) {
-    fuelEfficiency - ((speed - 60) * 2)
-  }
-  
-  var caption = document.getElementById('tripResult');
-
-  var duration = (distance / speed).toFixed(1);
-  var cost = (costPerGallon * (distance / fuelEfficiency)).toFixed(2);
-
-  caption.innerHTML = "The duration of your trip is " + duration + " hours and the costs are GBP " + cost + "!";
-}
-
-function fizzbuzz() {
-  var input = parseInt(document.getElementById('input').value);
-  var caption = document.getElementById('show');
-
-    if (input % 3 == 0 && input % 5 == 0) {
-       var show = 'Fizzbuzz';
-    } else if (input % 5 == 0) {
-       var show = 'Buzz';
-    } else if (input % 3 == 0) {
-        var show = 'Fizz';
-    } else { 
-       var show = input;
-    }
-
-  caption.innerHTML = show + "!";
-
+myApp.setup = function() {
+  var button = document.getElementById('calc');
+  button.addEventListener('click', myApp.calc);
 }
 
 
-
+document.addEventListener('DOMContentLoaded', myApp.setup);
 
 
 
