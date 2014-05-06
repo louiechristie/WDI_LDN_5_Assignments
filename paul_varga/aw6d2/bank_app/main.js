87cd8cd1790f040e35dcc82e05a640b640c6d647
paulvarga23 <paul_varga@me.com>
2014-05-06 20:16:48 +0100

    functionality working

--- paul_varga/aw6d2/bank_app/main.js
+++ paul_varga/aw6d2/bank_app/main.js
@@ -17,52 +17,92 @@ myApp.addToCurrent = function() {
 
 myApp.withdrawFromCurrent = function() {
   var input = parseInt($('#amountCurrent').val());
-  var balance = parseInt($('#currentOutput').val());
+  var currentbalance = parseInt($('#currentOutput').val());
+  var newbalance = currentbalance - input
+  var savingsbalance = parseInt($('#savingsOutput').val());
 
-  if (balance == parseInt(balance)) {
-    balance = parseInt($('#currentOutput').val());
+
+  if (currentbalance == parseInt(currentbalance)) {
+    currentbalance = parseInt($('#currentOutput').val());
   } else {
-      balance = 0;
+      currentbalance = 0;
   }
 
-  if (balance > 0) {
-    balance = $('#currentOutput').val(balance - input);
+  if (newbalance < 0 ){
+    alert("You cannot withdraw this amount, but we will try to access your savings.");
+    var newbalance = Math.abs(newbalance);
+    var newsavingsbalance = savingsbalance - newbalance;
+    console.log(newsavingsbalance);
+      if (newsavingsbalance < 0) {
+        alert("You do not have enough money in your savings. No withdrawl possible.");
+      } else {
+          $('#savingsOutput').val(savingsbalance - newbalance);
+          $('#currentOutput').val(currentbalance - currentbalance);
+      }
   } else {
-    balance = 0;
-    alert("You currently have no overdraft - call your bank manager.");
+    newbalance = $('#currentOutput').val(currentbalance - input);
   }
   
 }
 
+
+
+
+// if ($newsavingsbalance < 0) {
+// alert("You do not have enough money in your savings. Withdrawl cannot be made.");
+// } else {
+// document.getElementById('balance-savings').value=($savingsbalance - $newbalance);
+// document.getElementById('balance-current').value=($currentbalance - $currentbalance);
+// }
+// } else {
+// document.getElementById('balance-current').value=($currentbalance - $amount);
+// }
+// }
+
+//SAMS START
+
+// myApp.currentwithdraw = function() {
+
+// var $amount = parseInt($('#amount-current').val());
+// var $currentbalance = parseInt($('#balance-current').val());
+// var $newbalance = $currentbalance - $amount
+// var $savingsbalance = parseInt($('#balance-savings').val());
+
+// if ($newbalance < 0) {
+// alert("You cannot withdraw this much money from this account. We will attempt to take the remainder from your savings.");
+// var $newbalance = Math.abs($newbalance);
+// var $newsavingsbalance = $savingsbalance - $newbalance;
+
+
 myApp.addToSavings = function() {
   var input = parseInt($('#amountSavings').val());
-  var balance = parseInt($('#savingsOutput').val());
+  var savingsBalance = parseInt($('#savingsOutput').val());
 
-  if (balance == parseInt(balance)) {
-    balance = parseInt($('#savingsOutput').val());
+  if (savingsBalance == parseInt(savingsBalance)) {
+    savingsBalance = parseInt($('#savingsOutput').val());
   } else {
-      balance = 0;
+      savingsBalance = 0;
   }
 
-  $('#savingsOutput').val(balance + input);
+  $('#savingsOutput').val(savingsBalance + input);
   
 }
 
 
 myApp.withdrawFromSavings = function() {
   var input = parseInt($('#amountSavings').val());
-  var balance = parseInt($('#savingsOutput').val());
+  var savingsBalance = parseInt($('#savingsOutput').val());
 
-  if (balance == parseInt(balance)) {
-    balance = parseInt($('#savingsOutput').val());
+  if (savingsBalance == parseInt(savingsBalance)) {
+    savingsBalance = parseInt($('#savingsOutput').val());
   } else {
-      balance = 0;
+      savingsBalance = 0;
   }
 
-  if (balance > 0) {
-    balance = $('#savingsOutput').val(balance - input);
+  if (savingsBalance > 0) {
+    savingsBalance = $('#savingsOutput').val(savingsBalance - input);
   } else {
-    balance = 0;
+    savingsBalance = 0;
     alert("You currently have no overdraft - call your bank manager.");
   }
   
