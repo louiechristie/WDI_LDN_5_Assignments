1970e7494edd16e2c8484f1b31cf7d2119e3d180
hansss <hanakim@college.harvard.edu>
2014-05-07 18:26:02 +0100

    Working chat

--- Hana/w6d3/chat-startingpoint/public/gachat.js
+++ Hana/w6d3/chat-startingpoint/public/gachat.js
@@ -1,17 +1,57 @@
 $.ajax
 
-var Stocks = Stocks || {};
+var Messages = Messages || {};
+
+Messages.textSubmitHandler = function(ev){
+  ev.preventDefault();
+  
+  var message = $('#message').val();
+  var since = $('#since').val();
+  var username = $('#username').val();
+
+  var data = {message: message, username: username, since: since}
+
+  var ajaxOptions = {
+    url: '/chat',
+    type: 'POST',
+    data: data,
+    success: Messages.updatePage,
+    error: function(data){
+      console.error("Ajax request failed.");
+      console.log(data);}
+  };
+  $.ajax(ajaxOptions).done(Messages.updatePage);
+
+};
+
+Messages.updatePage = function(data){
+
+  console.log(data)
+  $.each(data, function(i, name)){
+
+    $("ul").append($('<li><span class="username">&lt;' + name['username'] + '&gt;</span><span class="message">' + name['message'] + '</span></li>'));
+    $("#since").val(name["timestamp"]);
+  });
+  };
+
+ 
+// $(".chat").append($('<li><span class="username">&lt;&gt;</span><span class="message"></span></li>'));
+// $("ul li .username").last().text($('#username').val());
+// $("ul li .message").last().text($('#message').val());
+// $lastMessage = $("ul li").last()  
+// $lastMessage.text()
 
 
-
-
-
-
-
-
-Stocks.setup = function() {
-  $('form').on('submit', Stocks.formSubmitHandler);
 };
 
 
-$(Stocks.setup)
+
+
+
+
+Messages.setup = function() {
+  $('form').on('submit', Messages.textSubmitHandler);
+};
+
+
+$(Messages.setup)
