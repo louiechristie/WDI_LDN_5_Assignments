47f6a38cca0305db4a462ab247af687077f8398c
louiechristie <post@louiechristie.com>
2014-05-07 21:19:40 +0100

    w6d3 send AJAX

---
+++ louiechristie/w6d3/chat/public/gachat.js
@@ -0,0 +1,77 @@
+var chatApp = chatApp || {};
+
+chatApp.getLines = function() {
+  console.log('getLines');
+  var lines = [];
+  return lines;
+
+}
+
+chatApp.messageSubmitHandler = function(ev) {
+  ev.preventDefault();
+  console.log('messageSubmitHandler');
+  var username = $('#username').val();
+  var message = $('#message').val();
+  var timestamp = Date.now();
+  console.log('message: '+message);
+  console.log('username: '+username);
+  console.log('timestamp: '+timestamp);
+
+  var ajaxOptions = {
+    url: '/',
+    username: username,
+    message: message,
+    since: timestamp,
+    type: 'GET',
+    success: chatApp.updatePage,
+    error: function(data) {
+      console.error("AJAX request failed.");
+      console.log(data);
+      },
+    };
+    $.ajax(ajaxOptions).done(chatApp.updatePage);
+}
+
+chatApp.updatePage = function (data) {
+  console.log('updatePage()');
+  var bits = ['username', 'message', 'since'];
+  $.each(bits, function(i, name) {
+  chatApp.updateBits($('chat #' + name), data[name]);
+  });
+}
+
+chatApp.updateBits = function(el, content) {
+  var $el = $(el);
+  $el.text(content);
+
+  var up = true;
+  var level = 15;
+  var step = function() {
+    var hex = level.toString(16);
+    $el.css('background-color', '#FAFA' + hex + hex);
+    if (up) {
+      if (level > 0) {
+        level--;
+        setTimeout(step, 25);
+      } else {
+        up = false;
+        level = 1;
+        setTimeout(step, 25);
+      }
+    } else if (level < 15) {
+      level++
+      setTimeout(step, 25);
+    } else {
+      $el.css('background-color', '#FFF');
+    }
+  };
+  setTimeout(step, 25);
+}
+
+
+chatApp.setup = function() {
+  $('[name=submit]').on('click', chatApp.messageSubmitHandler);
+};
+
+$(chatApp.setup);
+console.log('chatApp.setup');
