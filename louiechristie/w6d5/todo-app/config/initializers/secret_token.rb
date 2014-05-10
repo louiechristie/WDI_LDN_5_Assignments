fbc64bb6b0c5039da5e48ff3074c18a390273086
louiechristie <post@louiechristie.com>
2014-05-10 15:39:55 +0100

    w6d5 todo-app initial commit

---
+++ louiechristie/w6d5/todo-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoApp::Application.config.secret_token = '8f267089e2553d88557291bd240338c2172b03e09e506d4157f0697bae343a06a13fed9af129eceff5b92c5077a9076887de0a7f610dbb3cbe92f368e878b103'
