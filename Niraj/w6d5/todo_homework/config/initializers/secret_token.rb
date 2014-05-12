f8a21e08fcdbef71b3caba0813faab4b1bc9738f
NirajVyas <nirajvyas28@hotmail.com>
2014-05-12 09:59:57 +0100

    todo_homework

---
+++ Niraj/w6d5/todo_homework/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoHomework::Application.config.secret_token = '388bc91cde0a66ad47596315b9ab5faa8a673c7b384df40e135c3ee60d5576ccd20a731b1827275ea6aefc3c5062e96cca2000802adeeb1fdb61e88650c48fc3'
