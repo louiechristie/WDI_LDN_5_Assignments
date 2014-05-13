30ac6ff253d677b5a9bbcc87797c6a66dcee75c6
louiechristie <post@louiechristie.com>
2014-05-13 11:43:49 +0100

    w7d2 Authorization with devise, planets_app, cookbook_app initial commit

---
+++ louiechristie/w7d2/planets_app/app/controllers/planets_controller.rb
@@ -0,0 +1,23 @@
+class PlanetsController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  def index
+    @planets = Planet.all
+  end
+
+  def create
+   Planet.create(params["planet"])
+   redirect_to(planets_path)
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
+  def destroy
+    Planet.find(params[:id]).destroy
+    redirect_to(planets_path)
+  end
+
+end
