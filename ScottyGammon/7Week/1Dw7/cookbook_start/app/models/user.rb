fab4d485dd660ea0e3a8dc9c644a7dc7fe1c5ef6
ScottyGams <ScottyGammon@gmail.com>
2014-05-13 12:08:28 +0100

    using devise

---
+++ ScottyGammon/7Week/1Dw7/cookbook_start/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  attr_accessible :email, :name, :role, :password, :password_confirmation
+  has_many :recipes
+  def role?(role)
+    self.role == role.to_s
+  end
+end
