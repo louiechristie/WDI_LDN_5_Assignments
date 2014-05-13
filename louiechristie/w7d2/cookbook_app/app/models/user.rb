5754a27c8036ef182293be36d62dd40e69f5b4f6
louiechristie <post@louiechristie.com>
2014-05-13 12:12:26 +0100

    w7d2 cookbook_app after remove password_digest from User

--- louiechristie/w7d2/cookbook_app/app/models/user.rb
+++ louiechristie/w7d2/cookbook_app/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   attr_accessible :email, :name, :role, :password, :password_confirmation
--- louiechristie/w7d2/cookbook_app/db/schema.rb
+++ louiechristie/w7d2/cookbook_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131022014412) do
+ActiveRecord::Schema.define(:version => 20140513105105) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
@@ -44,10 +44,9 @@ ActiveRecord::Schema.define(:version => 20131022014412) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "role"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
