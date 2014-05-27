8b0d64f7822b84f6c266648ba5340f2be22945e4
NoorBL <noorbinladin@gmail.com>
2014-05-27 17:09:50 +0100

    creating and updating contacts

--- noorbl/w9/w9d2/backbone_rails_addressbook/app/assets/javascripts/people.js
+++ noorbl/w9/w9d2/backbone_rails_addressbook/app/assets/javascripts/people.js
@@ -2,6 +2,7 @@ var People = People || {
   Models: {},
   Collections: {},
   Views: {},
+  Routers: {},
   setup: function() {}
 }
 
@@ -14,12 +15,46 @@ People.Collections.PersonCollection = Backbone.Collection.extend({
 
 People.Views.PersonView = Backbone.View.extend({
   tagName: 'div',
+  events: {'click h2': 'makeNameEditable', 'blur input': 'saveChanges', 'keyup input': 'keyboardSaveChanges'},
   template: _.template($('#tmpl_person').html()),
+
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+
   render: function() {
     this.$el.html(this.template({person: this.model}));
     return this;
+  },
+
+  makeNameEditable: function(ev) {
+    var $target = $(ev.currentTarget);
+    var name = $target.text();
+    var newInput = $('<input>');
+    newInput.val(name);
+    $target.replaceWith(newInput);
+    newInput.focus();
+  },
+
+  keyboardSaveChanges: function(ev) {
+    if (13 == ev.keyCode) {
+      this.saveChanges(ev);
+    } else if (27 == ev.keyCode) {
+      this.render();
+    }
+  },
+
+  saveChanges: function(ev) {
+    var $target = $(ev.currentTarget);
+    var newName = $target.val();
+    this.model.set('name', newName);
+    this.model.save();
+    this.render();
   }
+
 });
+
+
 
 People.Views.PersonCollectionView = Backbone.View.extend({
   el: '#container',
@@ -34,12 +69,55 @@ People.Views.PersonCollectionView = Backbone.View.extend({
   }
 });
 
-People.setup = function() {
-  var allPeople = new People.Collections.PersonCollection();
-  allPeople.fetch({ success: function() {
-    var collectionView = new People.Views.PersonCollectionView({collection: allPeople});
+
+People.Views.PersonCreationView= Backbone.View.extend({
+  el: '#container',
+  events: {'submit form': 'createNewPerson'},
+  template: _.template($('#tmpl_newperson').html()),
+
+  render: function(){
+    this.$el.html(this.template());
+    return this;
+  },
+
+  createNewPerson: function(ev) {
+    ev.preventDefault();
+    var name = $('input[name="name"]').val();
+    var email = $('textarea[name="email"]').val();
+    var person = new People.Models.Person({name: name, email: email});
+    People.allPeople.add(person);
+    person.save();
+    People.router.navigate("", {trigger: true});
+  }
+});
+
+
+People.Routers.AppRouter = Backbone.Router.extend({
+  routes: {
+    "": "showIndex",
+    "new": "createNewPerson"
+  },
+  showIndex: function() {
+    var collectionView = new People.Views.PersonCollectionView({collection: People.allPeople});
     collectionView.render();
-  }});
+  },
+  createNewPerson: function() {
+    new People.Views.PersonCreationView().render();
+  }
+});
+
+
+
+
+
+
+
+
+People.setup = function() {
+  People.router = new People.Routers.AppRouter();
+  People.allPeople = new People.Collections.PersonCollection();
+  People.allPeople.fetch({ success: People.router.showIndex });
+  Backbone.history.start();
 };
 
 
--- noorbl/w9/w9d2/backbone_rails_addressbook/app/controllers/people_controller.rb
+++ noorbl/w9/w9d2/backbone_rails_addressbook/app/controllers/people_controller.rb
@@ -5,4 +5,32 @@ class PeopleController < ApplicationController
     @people = Person.all
     respond_with @people
   end
+
+ def create
+    @person = Person.new params[:person]
+    if @person.save
+      respond_with @person, status: :accepted
+    else
+      respond_with @person, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    @person = Person.find params[:id]
+    if @person.update_attributes params[:person]
+      respond_with @person, status: :accepted
+    else
+      respond_with @person, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+
+
+
+
+
 end
