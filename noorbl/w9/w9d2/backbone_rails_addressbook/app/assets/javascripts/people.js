8147a2c1365aab7e4a7ff7e62ffb338bb3e9add9
NoorBL <noorbinladin@gmail.com>
2014-05-27 12:32:44 +0100

    rendering data

---
+++ noorbl/w9/w9d2/backbone_rails_addressbook/app/assets/javascripts/people.js
@@ -0,0 +1,47 @@
+var People = People || {
+  Models: {},
+  Collections: {},
+  Views: {},
+  setup: function() {}
+}
+
+People.Models.Person = Backbone.Model.extend({});
+People.Collections.PersonCollection = Backbone.Collection.extend({
+  model: People.Models.Person,
+  url: '/people'
+});
+
+
+People.Views.PersonView = Backbone.View.extend({
+  tagName: 'div',
+  template: _.template($('#tmpl_person').html()),
+  render: function() {
+    this.$el.html(this.template({person: this.model}));
+    return this;
+  }
+});
+
+People.Views.PersonCollectionView = Backbone.View.extend({
+  el: '#container',
+  render: function() {
+    var that = this;
+    this.$el.empty();
+    this.collection.each(function(personModel) {
+      var personView = new People.Views.PersonView({model: personModel});
+      that.$el.append(personView.render().el);
+    });
+    return this;
+  }
+});
+
+People.setup = function() {
+  var allPeople = new People.Collections.PersonCollection();
+  allPeople.fetch({ success: function() {
+    var collectionView = new People.Views.PersonCollectionView({collection: allPeople});
+    collectionView.render();
+  }});
+};
+
+
+$(People.setup);
+
