2b088a65b4818f6db26c26be6bcbbb821801de68
samuel-hill <samuel_hill@me.com>
2014-05-27 16:19:25 +0100

    fixed error, missing end in the notes controller - all working.

--- samuel_hill/w9d2/backbone-notepad/app/assets/javascripts/notes.js
+++ samuel_hill/w9d2/backbone-notepad/app/assets/javascripts/notes.js
@@ -14,10 +14,43 @@ Notes.Collections.NoteCollection = Backbone.Collection.extend({
 
 Notes.Views.NoteView = Backbone.View.extend({
   tagName: 'div',
+  events: {'click h2': 'makeHeadlineEditable',
+          'blur input': 'saveChanges',
+          'keyup input': 'keyboardSaveChanges'
+  },
   template: _.template($('#tmpl_note').html()),
+
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+
   render: function() {
     this.$el.html(this.template({ note: this.model}));
     return this;
+  },
+
+  makeHeadlineEditable: function(ev) {
+    var $target = $(ev.currentTarget);
+    var headline = $target.text();
+    var newInput = $('<input>');
+    newInput.val(headline);
+    $target.replaceWith(newInput);
+    newInput.focus();
+  },
+
+  keyboardSaveChanges: function(ev) {
+    if (13 == ev.keyCode) {
+      this.saveChanges(ev);
+    } else if (27 == ev.keyCode) {
+      this.render();
+    }
+  },
+
+  saveChanges: function(ev) {
+    var $target = $(ev.currentTarget);
+    var newHeadline = $target.val();
+    this.model.set('title', newHeadline);
+    this.model.save();
   }
 });
 
@@ -37,12 +70,22 @@ Notes.Views.NoteCollectionView = Backbone.View.extend({
 // show the form for a new note
 Notes.Views.NoteCreationView = Backbone.View.extend({
   el: '#container',
-  events: {'click .close' : 'closeNote'},
+  events: {'submit form' : 'createNewNote'},
   template: _.template($('#tmpl_newnote').html()),
   render: function() {
     this.$el.html(this.template());
     return this;
-  }
+  },
+
+  createNewNote: function(ev) {
+    ev.preventDefault();
+    var title = $('input[name="title"]').val();
+    var body = $('textarea[name="body"]').val();
+    var note = new Notes.Models.Note({title: title, body: body});
+    Notes.allNotes.add(note);
+    note.save();
+    Notes.router.navigate("", {trigger: true});
+    }
 });
 
 Notes.Routers.AppRouter = Backbone.Router.extend({
--- samuel_hill/w9d2/backbone-notepad/app/controllers/notes_controller.rb
+++ samuel_hill/w9d2/backbone-notepad/app/controllers/notes_controller.rb
@@ -5,4 +5,23 @@ class NotesController < ApplicationController
     @notes = Note.all
     respond_with @notes
   end
+
+  def create
+    @note = Note.new params[:note]
+    if @note.save
+      respond_with @note, status: :accepted
+    else
+      respond_with @note, status: :unproccessable_entity
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update_attributes params[:note]
+      respond_with @note, status: :accepted
+    else
+      respond_with @note, status: :unproccessable_entity
+    end
+  end 
+
 end
