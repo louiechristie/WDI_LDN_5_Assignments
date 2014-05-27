0456af1989343a2c07606ebdc216abbd1f8a9f6f
jasonklein <jasonklein@gmail.com>
2014-05-27 05:36:14 +0100

    Add script templates for Planet and Star views, develop addBodyFromForm method.

--- jason_banks/w9d1/planets/js/celestials.js
+++ jason_banks/w9d1/planets/js/celestials.js
@@ -6,7 +6,7 @@ var celestialsApp = celestialsApp || {
 
 celestialsApp.models.Body = Backbone.Model.extend({
   defaults: {
-    type: 'Planet',
+    body_type: 'Planet',
     name: '',
     galaxy: '',
     mass: '',
@@ -59,29 +59,57 @@ celestialsApp.view.BodyCollectionView = Backbone.View.extend({
   initialize: function() {
     console.log('Initializing!');
     this.collection = new celestialsApp.collection.BodyCollection();
-    this.collection.bind('add', this.render, this);
+    this.collection.bind('addPlanet', this.render, this);
+    this.collection.bind('addStar', this.render, this);
   },
 
-  add: function(type, name, galaxy, mass, age) {
-    var body = new celestialsApp.model.Body ({type: type, name: name, galaxy: galaxy, mass: mass, age: age });
-    this.collection.add(body);
+  addPlanet: function(body_type, name, galaxy, mass, star, moons, age) {
+    var planet = new celestialsApp.model.Body ({body_type: body_type, name: name, galaxy: galaxy, mass: mass, star: star, moons: moons, age: age });
+    this.collection.add(planet);
+  },
+
+  addStar: function(body_type, name, galaxy, mass, classification, luminosity, color, age) {
+    var star = new celestialsApp.model.Body ({body_type: body_type, name: name, galaxy: galaxy, mass: mass, classification: classification, luminosity: luminosity, color: color, age: age });
+    this.collection.add(star);
   },
 
   addBodyFromForm: function(ev){
     ev.preventDefault();
-    var $type = $('#type');
+    var $body_type = $('#body_type');
     var $name = $('#name');
     var $galaxy = $('#galaxy');
     var $mass = $('#mass');
     var $age = $('#age');
-    this.add($type.val(), $name.val(), $galaxy.val(), $mass.val(), $age.val());
-    $type.val('');
+    var $star = $('#star');
+    var $moons = $('#moons');
+    var $classification = $('#classification');
+    var $luminosity = $('#luminosity');
+    var $color = $('#color');
+    if($body_type.val() === 'planet'){
+      this.addPlanet($body_type.val(), $name.val(), $galaxy.val(), $mass.val(), $star.val(), $moons.val(), $age.val());
+    } else {
+      this.addStar($body_type.val(), $name.val(), $galaxy.val(), $mass.val(), $classification.val(), $luminosity.val(), $color.val(), $age.val());
+    }
+    $body_type.val('');
     $name.val('');
     $galaxy.val('');
     $mass.val('');
     $age.val('');
+    $star('');
+    $moons('');
+    $classification('');
+    $luminosity('');
+    $color('');
+  },
+
+  render: function(){
+    $('#bodies').empty();
+    this.collection.each(function(el){
+      bodyView = new celestialsApp.views.PlanetView({model: planet})
+      $('#bodies').append(bodyView.render().el);
+    });
   }
-})
+});
 
 // var addressApp = addressApp || {
 //   models: {},
@@ -102,12 +130,12 @@ celestialsApp.view.BodyCollectionView = Backbone.View.extend({
 //   model: addressApp.models.Address
 // });
 
-addressApp.views.AddressView = Backbone.View.extend({
-  tagName: 'li',
-  events: {'click span': 'remove'},
-  remove: function() {
-    this.model.destroy();
-  },
+// addressApp.views.AddressView = Backbone.View.extend({
+//   tagName: 'li',
+//   events: {'click span': 'remove'},
+//   remove: function() {
+//     this.model.destroy();
+//   },
 //   render: function(){
 //     var templateContents = $('#tmpl_address').html();
 //     var template = _.template(templateContents);
@@ -116,43 +144,43 @@ addressApp.views.AddressView = Backbone.View.extend({
 //   }
 // })
 
-addressApp.views.AddressCollectionView = Backbone.View.extend({
-  el: '#container',
-  events: {'submit form#address_form': 'addAddressFromForm'},
+// addressApp.views.AddressCollectionView = Backbone.View.extend({
+//   el: '#container',
+//   events: {'submit form#address_form': 'addAddressFromForm'},
 
-  initialize: function() {
-    console.log("This is the initializing function.");
-    this.collection = new addressApp.collections.AddressCollection();
-    this.collection.bind('add', this.render, this);
-    this.collection.bind('remove', this.render, this);
-  },
+//   initialize: function() {
+//     console.log("This is the initializing function.");
+//     this.collection = new addressApp.collections.AddressCollection();
+//     this.collection.bind('add', this.render, this);
+//     this.collection.bind('remove', this.render, this);
+//   },
 
-  add: function(first_name, last_name, email, image){
-    var address = new addressApp.models.Address({first_name: first_name, last_name: last_name, email: email, image: image});
-    this.collection.add(address);
-  },
+  // add: function(first_name, last_name, email, image){
+  //   var address = new addressApp.models.Address({first_name: first_name, last_name: last_name, email: email, image: image});
+  //   this.collection.add(address);
+  // },
 
-  addAddressFromForm: function(ev) {
-    ev.preventDefault();
-    var $first_name = $('#first_name');
-    var $last_name = $('#last_name');
-    var $email = $('#email');
-    var $image = $('#image');
-    this.add($first_name.val(), $last_name.val(), $email.val(), $image.val());
-    $first_name.val('');
-    $last_name.val('');
-    $email.val('');
-    $image.val('');
-  },
+  // addAddressFromForm: function(ev) {
+  //   ev.preventDefault();
+  //   var $first_name = $('#first_name');
+  //   var $last_name = $('#last_name');
+  //   var $email = $('#email');
+  //   var $image = $('#image');
+  //   this.add($first_name.val(), $last_name.val(), $email.val(), $image.val());
+  //   $first_name.val('');
+  //   $last_name.val('');
+  //   $email.val('');
+  //   $image.val('');
+  // },
 
-  render: function(){
-    $('#address_container').empty();
-    this.collection.each(function(el){
-      var addressView = new addressApp.views.AddressView({ model: el});
-      $('#address_container').append(addressView.render().el);
-    });
-  }
-})
+//   render: function(){
+//     $('#address_container').empty();
+//     this.collection.each(function(el){
+//       var addressView = new addressApp.views.AddressView({ model: el});
+//       $('#address_container').append(addressView.render().el);
+//     });
+//   }
+// })
 
 addressApp.setup = function() {
   var addressBook = new addressApp.views.AddressCollectionView();
