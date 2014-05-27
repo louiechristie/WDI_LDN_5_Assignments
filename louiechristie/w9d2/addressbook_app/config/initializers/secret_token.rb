0eb7dfcf5b1e1665ca57a53b869c5a74a7ff9017
louiechristie <post@louiechristie.com>
2014-05-27 12:06:23 +0100

    w9d2 addressbook_app initial commit

---
+++ louiechristie/w9d2/addressbook_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressbookApp::Application.config.secret_token = '658eb79ed7b97b7b6d62228046cb1cc3fabebe17a8004c1166f503cc1ca0ad93144db79373c3da12bdf27721befb2d2a87b61d32df04e0a39347418d11882408'
