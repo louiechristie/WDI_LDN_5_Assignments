31bd37ad0f6d778ffe10605736812b92b6dae3c9
samuel-hill <samuel_hill@me.com>
2014-05-27 10:06:03 +0100

    initial commit with seed data

---
+++ samuel_hill/w9d2/backbone-notepad/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneNotepad::Application.config.secret_token = '9bf904272f4a00594c5c0d271114df772b8d87fd0b8a7a2785de800142b0f96eb0fb105bc90240d2c5781a651683b9bb7d2f193409a750b9209f07c31441195c'
