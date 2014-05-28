7c695ac7198c59e357e13008c0d1ee4ce405c979
mikemjharris <mike.mj.harris@gmail.com>
2014-05-28 09:20:17 +0100

    planets app

--- mikeharris/w8d2/planets_app/app/assets/javascripts/planets.js
+++ mikeharris/w8d2/planets_app/app/assets/javascripts/planets.js
@@ -13,25 +13,114 @@ Planets.Collections.PlanetCollection = Backbone.Collection.extend({
   url: '/planets'
 })
 
-Planets.Routers.AppRouter = Backbone.Router.extend({
-  routes: {
-    "": showIndex,
-    "new": newPlanet,
-  },
 
-  showIndex: function() {},
+Planets.Views.PlanetView = Backbone.View.extend({
+ tagName: 'div',
+ template: _.template($('#tmpl_planet').html()),
+ events: {
+  'click a': 'deletePlanet',
+  'dblclick': 'editPlanet'  
+    },
 
-  newPlanet: function() {},
+ render: function() {
+  this.$el.html(this.template({planet: this.model}));
+  return this
+ },
+
+ deletePlanet: function(ev) {
+  ev.preventDefault();
+  this.model.destroy();
+  new Planets.Views.PlanetCollectionView({collection: Planets.allPlanets}).render();
+ },
+
+ editPlanet: function(ev) {
+  var that = this
+    _.each(this.$('p'), function(el, i){
+    // $.each(this.$('p'), function(i, el) {
+      // console.log(i.text())
+       console.log(i)
+       console.log($(el).text())
+       // console.log(i)
+       // console.log(list)
+       // console.log(el.text);
+      var p_value = $(el).text();
+      var newInput = $('<input>').val(p_value);
+      $(el).replaceWith(newInput);
+   });
+  }
 
 })
 
+Planets.Views.PlanetCollectionView = Backbone.View.extend({
+  el: '#container',
+
+  render: function() {
+    var that = this;
+    this.$el.empty();
+    this.collection.each(function(planetModel){
+      var planetView = new Planets.Views.PlanetView({model: planetModel});
+      that.$el.append(planetView.render().el);
+    });
+    this.totalmoons();
+    return this;
+  },
+
+  totalmoons: function() {
+    console.log("test");
+    var total_moons = this.collection.pluck("moons")
+    $('#total_moons').html("total_moons:" + total_moons)
+  }
+})
+
+Planets.Routers.AppRouter = Backbone.Router.extend({
+  routes: {
+    "": 'showIndex',
+    "new": 'newPlanet',
+  },
+  showIndex: function() {
+    var collectionView = new Planets.Views.PlanetCollectionView({ collection: Planets.allPlanets });
+    collectionView.render();
+  },
+
+  newPlanet: function() {
+    var newplanetView = new Planets.Views.PlanetCreateView()
+    newplanetView.render();
+  },
+
+})
+
+Planets.Views.PlanetCreateView = Backbone.View.extend({
+  el: '#container',
+  template: _.template($('#tmpl_newplanet').html()),
+  events: {
+    'submit form': 'createPlanet'
+  },
+  render: function() {
+    this.$el.empty();
+    this.$el.html(this.template());
+  },
+
+  createPlanet: function(ev) {
+    ev.preventDefault();
+    var name = $("input[name='name']").val();
+    var size = $("input[name='size']").val();
+    var moons = $("input[name='moons']").val();
+    var dist_from_sun = $("input[name='dist_from_sun']").val();
+    var planet = new Planets.Models.Planet({name: name, size: size, moons: moons, dist_from_sun:dist_from_sun})
+    Planets.allPlanets.add(planet);
+    planet.save();
+    Planets.router.navigate("", {trigger: true});
+  }
+
+})
 
 Planets.setup = function () {
-  var planetsRouter = new Planets.Routers.AppRouter();
-  
-
-
+  Planets.router = new Planets.Routers.AppRouter();
+  Planets.allPlanets = new Planets.Collections.PlanetCollection();
+  Planets.allPlanets.fetch({
+    success: Planets.router.showIndex
+  });
   Backbone.history.start();
-
-
 }
+
+$(Planets.setup);
--- mikeharris/w8d2/planets_app/app/controllers/planets_controller.rb
+++ mikeharris/w8d2/planets_app/app/controllers/planets_controller.rb
@@ -4,8 +4,28 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
 
-
     respond_with @planets
+  end
+
+  def create 
+    @planet = Planet.new params[:planet]
+
+    if @planet.save 
+      respond_with @planet, status: :accepted
+    else
+      respond_with @planet, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @planet = Planet.find(params[:id])
+
+    if @planet.destroy
+      respond_with @planet, status: :accepted
+    else
+      respond_with @planet, status: :unprocessable_entity
+    end
+
   end
 
 end
