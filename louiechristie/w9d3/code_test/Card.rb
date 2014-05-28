50fa254700128c00d95f8d19340d9408da588ca6
louiechristie <post@louiechristie.com>
2014-05-28 16:02:29 +0100

    w9d3 credit card code test, amex test

--- louiechristie/w9d3/code_test/Card.rb
+++ louiechristie/w9d3/code_test/Card.rb
@@ -5,25 +5,76 @@ class Card
 
   def initialize(card_number)
     @card_number = card_number
+    @type
   end
 
 
   def check()
     
-    if is_a_number?(@card_number)
+    
+
+    if is_a_number?() && valid_type_of_card?() && valid_luhn_number?()
       return "valid"
     else
       return "invalid"
     end
+    // check is a valid type of card
+
+    // check Luhn alogrithm
+
   end
 
 
-  def is_a_number?(string)
+  def is_a_number?
     begin
-      !!Integer(string)
+      !!Integer(@card_number)
     rescue ArgumentError, TypeError
       false
     end
+  end
+
+  def valid_type_of_card?
+    case @card_number[0]
+    when 1..2
+      return false
+      break
+    when 3
+      return check_is_amex?
+    when 4
+      return check_is_visa?
+    when 5
+      return check_is_mastercard?
+    when 6 
+      return check_is_discover?
+    when 7..9
+      return false
+      break
+  end
+
+  def check_is_amex?
+    if 
+    (
+      @cardnumber[0] == 3
+      && (@card_number[1] == 4 || @card_number[1] == 7) 
+      && @cardnumber.length == 15
+    )
+      return true
+    else
+      return false
+    end
+
+  end
+
+  def check_is_visa?
+    return false
+  end
+
+  def check_is_mastercard?
+    return false
+  end
+
+  def check_is_discover?
+    return false
   end
 
 end
