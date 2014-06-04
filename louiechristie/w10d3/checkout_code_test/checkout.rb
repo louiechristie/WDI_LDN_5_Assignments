402455cc9d1d6aab4c5bf1042d7415268c9c4982
louiechristie <post@louiechristie.com>
2014-06-04 17:11:45 +0100

    calculating BOGOF

--- louiechristie/w10d3/checkout_code_test/checkout.rb
+++ louiechristie/w10d3/checkout_code_test/checkout.rb
@@ -2,12 +2,14 @@ class Checkout
 
   require 'pry'
 
-  attr_writer :total, :basket
-
+  attr_accessor :basket, :offers, :prices
+  attr_writer :total    #reader method defined below
 
   def initialize(pricing_rules)
     @total = 0
     @basket = {}
+    @offers = {}
+    @prices = { FR1: 3.11, SR1: 5, CF1: 11.23 }
 
     rules = pricing_rules.split(', ')
 
@@ -17,8 +19,9 @@ class Checkout
       
       case offer.split(" ").first
         when "BOGOF"
-
+          @offers[item] = "BOGOFF"
         when "3_OR_MORE"
+          @offers[item] = "3_OR_MORE"
       end
         
     end
@@ -33,17 +36,25 @@ class Checkout
   end
 
   def total
-    @basket.each do |key, value|
-      case key
+    @basket.each do |item, quantity|
+      case item
       when "FR1"
-        @total += value*3.11
+        if @offers[item] == "BOGOFF"
+          @total += (quantity / 2).floor*3.11 + (quantity % 2)*3.11
+        else
+          @total += quantity*3.11
+        end
       when "SR1"
-        @total += value*5.00
+        @total += quantity*5.00
       when "CF1"
-        @total += value*11.23
+        @total += quantity*11.23
       end
     end
     return @total
+  end
+
+  def calculate_price(item, quantity)
+
   end
 
 end
