a4e490b769578b2e11213ce088008b187b2035dd
louiechristie <post@louiechristie.com>
2014-06-04 17:51:14 +0100

    refactor offers

--- louiechristie/w10d3/checkout_code_test/checkout.rb
+++ louiechristie/w10d3/checkout_code_test/checkout.rb
@@ -9,21 +9,14 @@ class Checkout
     @total = 0
     @basket = {}
     @offers = {}
-    @prices = { FR1: 3.11, SR1: 5, CF1: 11.23 }
+    @prices = { "FR1" => 3.11, "SR1" => 5, "CF1" => 11.23 }
 
     rules = pricing_rules.split(', ')
 
     rules.each do |rule|
       item = rule.split(": ").first
       offer = rule.split(": ").last
-      
-      case offer.split(" ").first
-        when "BOGOF"
-          @offers[item] = "BOGOFF"
-        when "3_OR_MORE_DISC0.50"
-          @offers[item] = "3_OR_MORE_DISC0.50"
-      end
-        
+      @offers[item] = offer    
     end
   end
 
@@ -44,23 +37,21 @@ class Checkout
 
   def calculate_item_total(item, quantity)
 
-    case item
-      when "FR1"
-        calculate_BOGOFF_total(item, quantity)
-      when "SR1"
-        return quantity*5.00
-      when "CF1"
-        return quantity*11.23
-      end
+    case offers[item] 
+    when "BOGOF"
+      return calculate_BOGOFF_total(item, quantity)
+    else
+      return quantity*prices[item]
+    end
 
   end
 
   def calculate_BOGOFF_total(item, quantity)
-    if @offers[item] == "BOGOFF"
-          return ((quantity / 2).floor*3.11 + (quantity % 2)*3.11)
-        else
-          return quantity*3.11
-        end
+      (quantity / 2).floor*prices[item] + (quantity % 2)*prices[item]
+  end
+
+  def calculate_discount_total(item, quantity)
+
   end
 
 end
